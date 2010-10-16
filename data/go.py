1d19503d0fd3fbb66614b30b1fec63c95bd49951
matthew von rocketstein <photon.thug@gmail.com>
2010-10-16 02:52:39 -0700

    core functionality for rcvr, xmtr, and terminal

--- data/go.py
+++ data/go.py
@@ -1,34 +1,34 @@
 """ go.py: second phase init
 """
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = ''
+
+from twisted.internet import reactor
 from twisted.internet import stdio
-from twisted.protocols import basic
+from twisted.internet.protocol import Factory
+
+from cortex.core.rcvr import RCVR
 
 from cortex import core
+universe = core.universe
 
-class Terminal(basic.LineReceiver):
-    from os import linesep as delimiter
-
-    def connectionMade(self):
-        self.transport.write('>>> ')
-
-    def lineReceived(self, line):
-        try:
-            self.sendLine('Eval: ' + str(eval(line)))
-
-        except Exception,e:
-            self.sendLine('Error Evaluating.')
-
-            try:
-                exec(line)
-                self.sendLine('Executed..')
-                self.sendLine(str(locals()))
-            except Exception,e:
-                [self.sendLine(line) for line in str(e).split('\n')]
-        self.transport.write('>>> ')
+def go_django():
+    import django.core.handlers.wsgi
+    application = django.core.handlers.wsgi.WSGIHandler()
 
 def main():
-    stdio.StandardIO(Echo())
-    from twisted.internet import reactor
+    universe['terminal'] = core.Terminal()
+    stdio.StandardIO(universe['terminal'])
+
+    #pf = Factory()
+    #pf.clients = []
+    #pf.protocol = core.RCVR
+    #reactor.listenTCP(1234, pf)
+
+    from cortex.core.rcvr import FileIOFactory
+    fileio = FileIOFactory({})
+    reactor.listenTCP(1234, fileio)
+
     reactor.run()
 
 if __name__ == '__main__':
