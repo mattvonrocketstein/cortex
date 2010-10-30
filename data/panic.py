97357d906795741f41dd65be178c45ea2f633705
mvr <matthewvonrocketstein@gmail-dot-com>
2010-10-30 04:14:07 -0700

    temporary solution for the panic script.

--- data/panic.py
+++ data/panic.py
@@ -1,1 +1,12 @@
- print "NIY"
+
+import os
+print '-'*80
+_procs = "ps aux|grep matt|grep python|grep -v stat|grep -v grep|grep -v pbat|grep -v wic"
+procs = os.popen(_procs).read()
+print procs
+print '-'*80
+pids =  os.popen(_procs+"|awk '{print $2}'").read().split()
+print pids
+_pids = ' '.join(pids)
+print os.popen('kill -KILL '+_pids).read()
+
