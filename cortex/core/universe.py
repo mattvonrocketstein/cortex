ee87c094be322e0b5283f37307cdf36f6f8e73f6
Matthew von Rocketstein <matt@telemachus.(none)>
2010-11-06 02:41:49 -0700

    autoreloading works now, control-d exists nicely even with autodiscover mainloop running. the universe knows more about enumerating, starting, and stopping threads.

--- cortex/core/universe.py
+++ cortex/core/universe.py
@@ -5,21 +5,42 @@ import inspect
 import simplejson
 from tempfile import NamedTemporaryFile
 
+#from twisted.internet import gtk2reactor
+#gtk2reactor.install()
 from twisted.internet import reactor
 
 from cortex.util import Memoize
+from cortex.core.reloading import AutoReloader
 from cortex.core.util import report, console
 from cortex.core.atoms import AutonomyMixin, PerspectiveMixin
 from cortex.core.atoms import PersistenceMixin
-from cortex.core.services import ServiceManager
+from cortex.core.services import ServiceManager,Service
+
+def getAnswer(Q, yesAction=None, noAction=None, dispatcher=None):
+    """ getAnswer: ask a "yes or no" question, dispatch to an action
+        (possibly the empty-action) based on user-response on stdin
+
+          Some Shortcuts w/ Questions:
+             <Q> will be given a question mark if it doesn't have one,
+             <Q> capitalization will be formulated as if a book title
+    """
+    ans = "JUNK_DATA"
+    if not Q.endswith('?'): Q += '?'
+    question = Q.title()
+    question+=' [y/n] > '
+    while ans not in 'ynYN':
+        ans = raw_input(question)
+    if ans.lower()=='y':   ans = True;  result = yesAction and yesAction()
+    elif ans.lower()=='n': ans = False; result = noAction and noAction()
+    return ans,result
 
 
-class __Universe__(object, AutonomyMixin, PerspectiveMixin,
+class __Universe__(AutoReloader, AutonomyMixin, PerspectiveMixin,
                    PersistenceMixin):
     """
         NOTE: this should effectively be a singleton
     """
-
+    testing = 131231111115
     node_list = []
     _services = []
 
@@ -78,17 +99,32 @@ class __Universe__(object, AutonomyMixin, PerspectiveMixin,
         return self.read_nodeconf()
 
     @property
+    def threads(self):
+        import threading
+        return threading.enumerate()
+
+    @property
     def nodes(self):
         """ nodes: dynamic definition """
         return self.node_list
 
+    def stop(self):
+        for service in self.services:
+            try: service.stop()
+            except Exception,e:
+                print 'exc',e
+                #IP.quitting=True
+        self.started = False
+        for thr in self.threads:
+            thr._Thread__stop()
+        self.reactor.stop()
     def play(self):
         """
             Post:
                 self.node_list = [ <list of active nodes> ]
         """
         report("Universe.play!")
-
+        self.started = True
         # Starts all nodes registered via the nodeconf
         if hasattr(self, 'nodeconf_file') and self.nodeconf_file:
             for node in self.read_nodeconf():
@@ -99,12 +135,38 @@ class __Universe__(object, AutonomyMixin, PerspectiveMixin,
 
         # Start special services provided by the universe
         for service in self.Services:
-            self._services.append(service(universe=self).play())
+            report('launching service',service)
+            self.loadService(service)
 
-        report('running reactor')
 
+        #report('running reactor')
         # Main loop
-        #reactor.run()
+        reactor.run()
+    def loadService(self,service):
+        """ """
+        if isinstance(service, str):
+            if "." in service:
+                raise Exception,'will not interpret dotpath yet'
+            else:
+                import inspect
+                ns={}
+                exec('from cortex.core.services import '+service+' as mod',ns)
+                mod = ns['mod']
+                for name in dir(mod):
+                    val = getattr(mod,name)
+                    if inspect.isclass(val):
+                        if not val==Service and issubclass(val, Service):
+                            #launch_service = lambda: val(universe=self).play
+                            print 'discovered service in ', mod.__name__,'@',mod.__file__
+                            ret = val(universe=self).play()
+                            self._services.append(ret)
+                            return ret
+                            #getAnswer('launch service@'+str([name, val]),
+                            #          yesAction=val(universe=self).play)
+        else:
+            ret = service(universe=self).play()
+            self._services.append(ret)
+            return ret
 
     @property
     @Memoize
@@ -128,7 +190,9 @@ class __Universe__(object, AutonomyMixin, PerspectiveMixin,
         from cortex.core.services.terminal import Terminal
         from cortex.core.services.beacon import Beacon
         from cortex.core.services._linda import Linda
-        return [Linda,Terminal ] #Beacon]
+        _Services = [Linda, Terminal]
+        # _Services.append(Beacon)
+        return _Services
 
     def django_service(self):
         """  Start special services provided by the universe """
