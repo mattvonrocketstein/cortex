25b8d72a8f982ba5cc2ab97d9ec7ffc87a187475
Matthew von Rocketstein <matt@telemachus.(none)>
2010-11-10 01:40:33 -0800

    saner PeerManager, working (but non-lazy) jsonrpc client api

--- cortex/core/peer.py
+++ cortex/core/peer.py
@@ -3,15 +3,34 @@
 
 from cortex.core.node import Node
 from cortex.core.manager import Manager
+from cortex.core.hds import HDS
+from cortex.core.util import report
 
 class PeerManager(Manager):
     """ """
 
-    class Peer(object):
-        pass
+    class asset_class(HDS):
+        """ """
 
+        def __str__(self):
+            port = str(getattr(self,'port','00'))
+            addr = getattr(self,'address','0')
+            return 'Peer@' + addr + ':' + port
+        __repr__ = __str__
+
+        def api(self, name, *args):
+            """ a jsonrpc client, to a remote jsonrpc server
+
+                  TODO: make this return an HDS that coerces values JIT
+            """
+            from twisted.internet import reactor
+            from txjsonrpc.netstring.jsonrpc import Proxy
+            proxy = Proxy(self.address, 1337)
+            return proxy.callRemote(name, *args).addCallbacks(PeerManager.printValue,
+                                                              PeerManager.printError)
+
+    def printValue(self, value): report("Result:", str(value))
+    def printError(self, error): report('error', error)
     def __iter__(self):
         """ dumb proxy """
         return Manager.__iter__(self)
-
-
