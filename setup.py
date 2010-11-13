118088f43855bb03ce697918ddabce599e30d2a3
mvr <matthewvonrocketstein@gmail-dot-com>
2010-11-13 00:39:17 -0500

    [master] Setuptools

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
+#from distutils.core import setup
+from setuptools import setup
 
-from distutils.core import setup
-#package_dir = {'': 'lib'}
-setup(name='cortex',
-      version='.1',
-      description = 'playground for distributed computing',
-      author      = 'mattvonrocketstein, in the gmails',
-      url         = 'one of these days',
-      packages    = ['cortex'],
-      scripts     = [ 'data/_scripts/go',
-                      'data/_scripts/go.py', ]
-     )
+
+setup(
+    name='cortex',
+    version='.1',
+    description = 'playground for distributed computing',
+    author      = 'mattvonrocketstein, in the gmails',
+    url         = 'one of these days',
+
+    packages    = ['cortex'],
+
+    scripts     = [ 'data/_scripts/go',
+                    'data/_scripts/go.py',
+                    'data/_scripts/panic',
+                    'data/_scripts/panic.py',]
+)
