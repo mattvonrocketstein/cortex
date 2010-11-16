98464ee4614a02236a6f91f80ef312b614d64409
mvr <matthewvonrocketstein@gmail-dot-com>
2010-11-15 23:01:29 -0500

    [bootstrap] Setup has a bootstrap

--- setup.py
+++ setup.py
@@ -1,19 +1,62 @@
 #!/usr/bin/env python
-#from distutils.core import setup
+import os, sys
+import subprocess
 from setuptools import setup
+from optparse import OptionParser
+
+BASEDIR = os.path.dirname(os.path.realpath(__file__))
+
+def boot(opts, *args, **options):
+    do = lambda things: subprocess.Popen(
+                                    'bash',
+                                     cwd=BASEDIR, shell=True, universal_newlines=True,
+                                     env=os.environ,
+                                     stdout=sys.stderr, stderr=sys.stderr, stdin=subprocess.PIPE
+                                    ).communicate(things)
+
+    sys.stderr.write("=> Making env {name}\n".format(name=opts.name))
+    do('virtualenv --no-site-packages {name}'.format(name=opts.name))
+    sys.stderr.write("=> Installing reqs\n")
+    do('source {name}/bin/activate && pip install -E {name} -r requirements.txt'.format(name=opts.name))
+    sys.stderr.write("=> Installing self\n")
+    do('source {name}/bin/activate && python setup.py {arguments}'.format(name=opts.name,
+                                                                          arguments=opts.develop and "develop" or
+                                                                                    "install"))
 
 
-setup(
-    name='cortex',
-    version='.1',
-    description = 'playground for distributed computing',
-    author      = 'mattvonrocketstein, in the gmails',
-    url         = 'one of these days',
+    if options.get('exit', False): exit(0)
 
-    packages    = ['cortex'],
+if __name__=='__main__':
+    parser = OptionParser()
+    parser.add_option('--boot', '-b', dest="boot",
+                      action="store_true",
+                      default=False,
+                      help="Bootstarp Cortex")
+    parser.add_option('--name', '-n', dest="name",
+                      default="node",
+                      help="Name of environment to build [default: node]")
+    parser.add_option("--permanent", '-p', dest="develop",
+                      action='store_false',
+                      default=True,
+                      help="Install cortex as a library instead of for development (symlink here)")
 
-    scripts     = [ 'data/_scripts/go',
-                    'data/_scripts/go.py',
-                    'data/_scripts/panic',
-                    'data/_scripts/panic.py',]
-)
+    parser
+    (options, args) = parser.parse_args()
+
+    if options.boot: boot(options, *args, exit=True)
+
+    setup(
+        name='cortex',
+        version='.1',
+        description = 'playground for distributed computing',
+        author      = 'mattvonrocketstein, in the gmails',
+        url         = 'one of these days',
+
+        packages    = ['cortex'],
+
+        scripts     = [ 'data/_scripts/go',
+                        'data/_scripts/go.py',
+                        'data/_scripts/panic',
+                        'data/_scripts/panic.py',]
+    )
+
