8cf7be037c05ad92feadb5d133bd65a092f0d18b
Matt <matt.anderson@cmgdigital.com>
2010-11-17 14:04:34 -0500

    removed inlined peer

--- lib/cortex/core/peer.py
+++ lib/cortex/core/peer.py
@@ -5,25 +5,16 @@ from cortex.core.node import Node
 from cortex.core.manager import Manager
 from cortex.core.hds import HDS
 from cortex.core.util import report
+class Peer(HDS):
+    """ """
 
-class PeerManager(Manager):
-    """
-
-        peerMan = PeerManager()
-        bob     = peerMan.register('bob',**bob_attributes)
-
-    """
-
-    class asset_class(HDS):
-        """ """
-
-        def __str__(self):
+    def __str__(self):
             port = str(getattr(self,'port','00'))
             addr = getattr(self,'address','0')
             return 'Peer@' + addr + ':' + port
-        __repr__ = __str__
+    __repr__ = __str__
 
-        def api(self, name, *args):
+    def api(self, name, *args):
             """ a jsonrpc client, to a remote jsonrpc server
 
                   TODO: make this return an HDS that coerces values JIT
@@ -33,6 +24,18 @@ class PeerManager(Manager):
             proxy = Proxy(self.address, 1337)
             return proxy.callRemote(name, *args).addCallbacks(PeerManager.printValue,
                                                               PeerManager.printError)
+
+class PeerManager(Manager):
+    """
+        Example usage:
+
+          peerMan = PeerManager()
+          bob     = peerMan.register('bob',**bob_attributes)
+          bob.api.load_service('beacon')
+
+    """
+
+    asset_class = Peer
 
     def printValue(self, value): report("Result:", str(value))
     def printError(self, error): report('error', error)
