5db3f2fbaddb63cdb0aecd4b2ce14bf72f6a6bff
Matt <matt.anderson@cmgdigital.com>
2010-11-17 07:41:04 -0500

    abstracted TupleBus so that the PostOffice is easier to understand. other services (all nodes/agents?) may also want to subclass TupleBus.

--- lib/cortex/services/_bus.py
+++ lib/cortex/services/_bus.py
@@ -6,40 +6,32 @@ from cortex.core.util import report
 from cortex.core.ground import Keyspace
 from cortex.services import Service
 
-class PostOffice(Service, Keyspace, Bus):
-    """ PostOffice Service: a thin wrapper over cyrusbus's basic layout
-          start: brief description of service start-up here
-          stop:  brief description service shutdown here
+class TupleBus(Bus):
+    """ a wrapper over cyrusbus to use tuples
+         (by default it uses lists and dictionaries)
+
+        cyrusbus also uses a "subscriptions object",
+        which is simply a key/callback dictionary.  this
+        version simplifies that by just storing callbacks.
     """
-
-    def __init__(self, *args, **kargs):
-        """
-        """
-        Service.__init__(self, *args, **kargs)
-        keyspace_name  = self.universe or 'CortexBus::keyspace'
-        keyspace_owner = self
-        Keyspace.__init__(self, keyspace_owner, name=keyspace_name)
-        Bus.__init__(self) # will call self.reset()
-
-    def reset(self):
-        """ override from cyrusbus to replace a normal dictionary with
-            the keyvalue wrapper over the lindypy-backed tuplespace
-        """
-        self.subscriptions = self # hack to make the bus use the Keyspace
 
     def unsubscribe(self, key, callback):
         if not self.has_subscription(key, callback):
             return self
-        self[key] = tuple([cb for cb in self[key] if cb!=callback])
+        self.subscriptions[key] = tuple([cb for cb in self.subscriptions[key] if cb!=callback])
 
     def has_subscription(self, key, callback):
-        """ override from cyrusbus """
-        if key not in self.public_keys():
+        """ override from cyrusbus enforcing simple callbacks as
+            subscriptions (by default cyrusbus uses dictionaries)
+        """
+        if key not in self.keys():
             return False
-        return callback in self[key]
+        return callback in self.subscriptions[key]
 
     def publish(self, key, *args, **kwargs):
-        """ override from cyrusbus enforcing simple callbacks as subscriptions
+        """ override from cyrusbus enforcing simple callbacks as
+            subscriptions (by default cyrusbus uses dictionaries)
+
         """
         if not self.has_any_subscriptions(key):
             return self
@@ -48,22 +40,58 @@ class PostOffice(Service, Keyspace, Bus):
             subscriber(self, *args, **kwargs)
 
     def subscribe(self, key, callback, force=False):
-        """ override from cyrusbus forcing tuples, not lists
-            also changed return-value, now sends all subscribers
+        """ override from cyrusbus forcing tuples, not lists.
+             also changed return-value, now sends back all subscribers for <key>
         """
-        subscription = callback
-
         if key not in self.public_keys():
-            self[key] = tuple([subscription])
+            self[key] = tuple([callback])
 
         elif force or not self.has_subscription(key, callback):
-            self[key] = self[key] + (subscription,)
+            self[key] = self[key] + (callback,)
         return self[key]
 
+class SelfHostingTupleBus(TupleBus):
+    """ This class fools cyrusbus into using instances directly in the
+        place of a simple dictionary -- convenient if your class already
+        speaks the dictionary protocol.
+
+            TODO: figure out how to *fully* reset..
+    """
+    def reset(self):
+        self.subscriptions = self
+
+
+class PostOffice(Service, Keyspace, SelfHostingTupleBus):
+    """ PostOffice Service: a wrapper over cyrusbus's basic layout
+
+          start: brief description of service start-up here
+          stop:  brief description service shutdown here
+
+        TODO: either wrap <publish> up in something asynchronous,
+              or guarantee that subscriber-callbacks are themselves
+              non-blocking.
+    """
+
+    def __init__(self, *args, **kargs):
+        """ """
+        Service.__init__(self, *args, **kargs)
+
+        default_name   = 'PostOffice::{_id}::keyspace'.format(_id=str(id(self)))
+        keyspace_name  = self.universe or default_name
+        keyspace_owner = self
+        Keyspace.__init__(self, keyspace_owner, name=keyspace_name)
+
+        Bus.__init__(self) # will call self.reset()
+
+    def msg(self, *args, **kargs):
+        """ TODO: determine caller function and dispatch to publish
+        """
+        NIY
+
     def start(self):
-        return self.reset()
+        """ """
+        self.reset()
 
     def play(self):
+        """ """
         return self
-
-    #keyspace_keys = Service.keys
