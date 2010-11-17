f2ce88370ab6da2cfb8192033b8430a2d11ef700
mvr <matthewvonrocketstein@gmail-dot-com>
2010-11-17 09:08:01 -0500

    better organization in file; added new constants for postoffice psuedo-types

--- lib/cortex/core/data.py
+++ lib/cortex/core/data.py
@@ -1,12 +1,27 @@
 """ cortex.core.data
 """
 
+# pseudo-types for message-passing.. mainly used in conjunction with postoffice
+################################################################################
+WARN_T   = "WARN_T"
+NOTICE_T = "NOTICE_T"
+EVENT_T  = "EVENT_T"
+PEER_T   = "PEER_T"
+
+# reflections on cortex codebase
+################################################################################
 SERVICES_DOTPATH = 'cortex.services'
 
+# generic networking constants
+################################################################################
 GENERIC_LOCALHOST = "0"
 LOOPBACK_HOST     = '127.0.0.1'
 DEFAULT_HOST      = LOOPBACK_HOST
+
+# cortex.services.terminal
+################################################################################
 IPY_ARGS          = ['-noconfirm_exit']
 
-# Things will break if this is changed?
-AVAHI_TYPE = "_http._tcp"
+# cortex.services.contrib
+################################################################################
+AVAHI_TYPE = "_http._tcp" # used in the old (avahi/dbus) peer-discovery strategy
