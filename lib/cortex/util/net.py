824db2ea6ab5faf5688c8adcd5772be53b1fb262
mvr <matthewvonrocketstein@gmail-dot-com>
2010-11-18 19:38:02 -0500

    stubbing out a few options for grabbing ip address

---
+++ lib/cortex/util/net.py
@@ -0,0 +1,88 @@
+""" cortex.util.net
+"""
+
+import socket
+
+def win_ipaddr():
+    """ stolen from stackoverflow.. this might be
+        useful for windows one of these days
+    """
+    from ctypes import Structure, windll, sizeof
+    from ctypes import POINTER, byref
+    from ctypes import c_ulong, c_uint, c_ubyte, c_char
+    MAX_ADAPTER_DESCRIPTION_LENGTH = 128
+    MAX_ADAPTER_NAME_LENGTH = 256
+    MAX_ADAPTER_ADDRESS_LENGTH = 8
+    class IP_ADDR_STRING(Structure):
+        pass
+    LP_IP_ADDR_STRING = POINTER(IP_ADDR_STRING)
+    IP_ADDR_STRING._fields_ = [
+        ("next", LP_IP_ADDR_STRING),
+        ("ipAddress", c_char * 16),
+        ("ipMask", c_char * 16),
+        ("context", c_ulong)]
+    class IP_ADAPTER_INFO (Structure):
+        pass
+    LP_IP_ADAPTER_INFO = POINTER(IP_ADAPTER_INFO)
+    IP_ADAPTER_INFO._fields_ = [
+        ("next", LP_IP_ADAPTER_INFO),
+        ("comboIndex", c_ulong),
+        ("adapterName", c_char * (MAX_ADAPTER_NAME_LENGTH + 4)),
+        ("description", c_char * (MAX_ADAPTER_DESCRIPTION_LENGTH + 4)),
+        ("addressLength", c_uint),
+        ("address", c_ubyte * MAX_ADAPTER_ADDRESS_LENGTH),
+        ("index", c_ulong),
+        ("type", c_uint),
+        ("dhcpEnabled", c_uint),
+        ("currentIpAddress", LP_IP_ADDR_STRING),
+        ("ipAddressList", IP_ADDR_STRING),
+        ("gatewayList", IP_ADDR_STRING),
+        ("dhcpServer", IP_ADDR_STRING),
+        ("haveWins", c_uint),
+        ("primaryWinsServer", IP_ADDR_STRING),
+        ("secondaryWinsServer", IP_ADDR_STRING),
+        ("leaseObtained", c_ulong),
+        ("leaseExpires", c_ulong)]
+    GetAdaptersInfo = windll.iphlpapi.GetAdaptersInfo
+    GetAdaptersInfo.restype = c_ulong
+    GetAdaptersInfo.argtypes = [LP_IP_ADAPTER_INFO, POINTER(c_ulong)]
+    adapterList = (IP_ADAPTER_INFO * 10)()
+    buflen = c_ulong(sizeof(adapterList))
+    rc = GetAdaptersInfo(byref(adapterList[0]), byref(buflen))
+    if rc == 0:
+        for a in adapterList:
+            adNode = a.ipAddressList
+            while True:
+                ipAddr = adNode.ipAddress
+                if ipAddr:
+                    yield ipAddr
+                adNode = adNode.next
+                if not adNode:
+                    break
+
+def ipaddr_with_internet():
+    """ you need to make an outbound connection to get
+        an ip-address on a particular interface.. this
+        function requires interwebs
+    """
+    port = 80
+    host = "gmail.com"
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect((host, port))
+    return s.getsockname()
+
+def ipaddr_hosts():
+    """ on linux, uses /etc/hosts, returns something like this:
+
+         ('cormac', ['localhost', 'testserver'], ['127.0.0.1'])
+    """
+    name, aliaslist, addresslist = socket.gethostbyname_ex(socket.gethostname())
+    return     name, aliaslist, addresslist
+
+def ipaddr_basic():
+    """ two different approaches just for reference purposes..
+        these results are always the same for me, but ymmv depending
+        on setup and platform..
+    """
+    print socket.gethostbyname(socket.gethostname())
+    print socket.gethostbyname(socket.getfqdn())
