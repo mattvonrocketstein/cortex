7ab9efe86f471891eb6622b63149e860e3dd554b
mvr <matthewvonrocketstein@gmail-dot-com>
2010-11-18 19:37:10 -0500

    better network mapper

--- lib/cortex/core/data.py
+++ lib/cortex/core/data.py
@@ -3,10 +3,10 @@
 
 # pseudo-types for message-passing.. mainly used in conjunction with postoffice
 ################################################################################
-WARN_T   = "WARN_T"
 NOTICE_T = "NOTICE_T"
 EVENT_T  = "EVENT_T"
 PEER_T   = "PEER_T"
+WARN_T   = "WARN_T"
 
 # reflections on cortex codebase
 ################################################################################
@@ -14,6 +14,7 @@ SERVICES_DOTPATH = 'cortex.services'
 
 # generic networking constants
 ################################################################################
+CORTEX_PORT_RANGE = (1337, 1473)
 GENERIC_LOCALHOST = "0"
 LOOPBACK_HOST     = '127.0.0.1'
 DEFAULT_HOST      = LOOPBACK_HOST
--- lib/cortex/services/terminal.py
+++ lib/cortex/services/terminal.py
@@ -1,5 +1,19 @@
 """ cortex.services.terminal
       see also: http://ipython.scipy.org/moin/Cookbook/JobControl
+
+        import sys
+        from IPython.Debugger import Pdb
+        from IPython.Shell import IPShell
+        from IPython import ipapi
+
+        shell = IPShell(argv=[''])
+
+        def set_trace():
+            ip = ipapi.get()
+            def_colors = ip.options.colors
+            Pdb(def_colors).set_trace(sys._getframe().f_back)
+
+
 """
 
 
