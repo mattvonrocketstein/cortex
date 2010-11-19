fb1645ce3b8d3c2ad8171e8803f4627980cb520d
Yaroslav Shirokov <slava@hackinggibsons.com>
2010-11-18 22:46:48 -0500

    [mudpee] Spamming advertisements

--- lib/cortex/services/mudpee.py
+++ lib/cortex/services/mudpee.py
@@ -3,9 +3,39 @@
 
 from cortex.services import Service
 from cortex.core.util import report, console
+from cortex.core.universe import Universe
+
+from twisted.internet.protocol import DatagramProtocol
 
 GROUP = '227.3.3.7'
 PORT = 31337
+
+class Advertiser(DatagramProtocol):
+    def __init__(self, universe, group=GROUP, port=PORT):
+        self.universe = universe
+        self.group = group
+        self.port = port
+        self.running = False
+
+    def advertise(self):
+        if not self.running:
+            report("WARNING", "Trying to advertise while not running")
+            return
+        report("TODO: Advertising!")
+        print "Writing: %d to %s %s" % (self.transport.write("Hello world", (self.group, self.port)), self.group, self.port)
+
+        if self.running:
+            self.universe.reactor.callLater(3, self.advertise)
+
+    def startProtocol(self):
+        self.running = True
+        self.transport.joinGroup(self.group).addBoth(lambda *args, **kwargs: report("Join: ", *args, **kwargs))
+        self.advertise()
+
+    def stopProtocol(self):
+        self.running = False
+        self.transport.leaveGroup(self.group).addBoth(lambda *args, **kwargs: report("Join: ", *args, **kwargs))
+
 
 class MudPee(Service):
     """
@@ -30,8 +60,12 @@ class MudPee(Service):
         """ """
         Service.stop(self)
         report('Stopping MudPee', self)
+        self.advertiser.stopListening().addCallback(report)
 
     def start(self):
         """
         """
         report('Starting MudPee', self)
+        advert = Advertiser(Universe)
+        self.advertiser = Universe.reactor.listenMulticast(PORT, advert, listenMultiple=True)
+        report("Advertiser:", self.advertiser)
