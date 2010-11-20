0b3fed2fe010b14faf674a3eb92b5cc96a7c9041
Matt <mattvonrocketstein@gmail.com>
2010-11-20 01:02:34 -0500

    cleaning cruft

--- lib/cortex/core/node.py
+++ lib/cortex/core/node.py
@@ -8,51 +8,34 @@ from cortex.core.data import LOOPBACK_HOST, GENERIC_LOCALHOST
 from cortex.core.atoms import AutonomyMixin, PerspectiveMixin
 from cortex.core.data import DEFAULT_HOST
 from cortex.core.util import report
-
-class MobileCodeMixin(object):
-    """ """
-    @property
-    def is_local(self):
-        """ """
-        return self.host in [LOOPBACK_HOST, GENERIC_LOCALHOST]
+from cortex.core.mixins import MobileCodeMixin
 
 class Node(MobileCodeMixin, AutonomyMixin, PerspectiveMixin):
     """
     """
-    def __init__(self, host=None, universe=None,
-                 name=None, resource_description=None, **kargs):
+    def __init__(self, host=None, universe=None, name=None, **kargs):
         """
         """
         self.universe = universe
         self.host = host or DEFAULT_HOST
         self.name = name
-        self.resource_description = resource_description
 
         # pass the remainder of the kargs to _post_init for subclassers
         if hasattr(self,'_post_init'):
             self._post_init(**kargs)
 
-    @property
-    def cortex_instance(self):
-        return self.universe.instance_dir
-
-    def __str__(self):
+    def asdf__str__(self):
         """ """
         nayme=self.name
         names = dict(nayme     = nayme,
                      parent   = self.__class__.__bases__[0].__name__,
                      host     = str(self.host),
                      instance = str(self.cortex_instance),)
-                     #resource_descr = self.__render_resource_description())
         return "<{nayme}-{parent}@{host}::{instance}>".format(**names)
 
-    def __render_resource_description(self):
-        """ OBSOLETE? """
-        return str(self.resource_description)
-
     def stop(self):
-        report("node::stopping")
-        super(Node,self).stop()
+        #report("node::stopping")
+        super(Node, self).stop()
 
     def play(self):
         """ CONVENTION:
@@ -63,7 +46,5 @@ class Node(MobileCodeMixin, AutonomyMixin, PerspectiveMixin):
 
             NOTE: overrides AutonomyMixin.play
         """
-        super(Node,self).play()
-        for item in self.resource_description.items():
-            print '\tevaluating resource:',item
+        super(Node, self).play()
         return self
