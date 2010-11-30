6b27eac0ba1bbce0d78d5b3c8af6d0af71358c6a
Matt <mattvonrocketstein@gmail.com>
2010-11-29 23:52:59 -0500

    major changes to iron out the abstract manager api and make subclasses conform

--- lib/cortex/core/manager.py
+++ lib/cortex/core/manager.py
@@ -5,10 +5,9 @@
 
 import datetime
 
-from cortex.core.hds import HierarchicalData
 from cortex.core.util import report
+from cortex.core.hds import HierarchicalData
 
-default_asset_class = HierarchicalData
 class Manager(object):
     """ Managers are inspired by django's managers.  Think of
         this class as a collection of patterns in tracking,
@@ -33,8 +32,72 @@ class Manager(object):
          NOTE: if subclasses define 'asset_class', then it will be used in place of
                the default class <HierarchicalData>
     """
-
     class NotFound(Exception): pass
+
+    def __init__(self, *args, **kargs):
+        """
+             NOTE: subclasses are discouraged from using args..
+                   why not be a good chap and use kargs instead?
+        """
+        self._pending   = []
+        self.registry   = {}
+
+        # generic storage for the actual manager itself.
+        self.generic_store = HierarchicalData()
+
+    def resolve_boot_order(self, **kargs):
+        """ by default, simply returns the names in
+            the order they were registered """
+        return [pending[0] for pending in self._pending ]
+
+    def load(self):
+        """ Convention:
+              if <manage> is used, this should be called after
+              all calls to it are finished
+
+              TODO: refactor
+        """
+        boot_order = self.resolve_boot_order()
+        report('determined boot order:', boot_order)
+        self.load_items(boot_order)
+
+    def load_items(self, items):
+        """ load_items """
+        for name in items:
+            search = filter(lambda x: name == x[0], self._pending)
+            if search:
+                junk_name, kls, kls_kargs = search[0]
+                kls_kargs = kls_kargs or {}
+                self.load_item(name=name, kls=kls, kls_kargs=kls_kargs,
+                               index=items.index(name))
+
+    def load_item(self, name=None, kls=None, kls_kargs=None, index=None):
+        """ will be called by Manager.load
+        """
+        obj = self.load_obj(kls=kls, **kls_kargs)
+
+        self.register(name,
+                      obj        = obj,
+                      index      = index,
+                      kargs      = kls_kargs)
+
+    def pre_load_obj(self, kls=None, **kls_kargs):
+        """ pre_load_obj hook:
+        """
+        return kls, kls_kargs
+
+    def load_obj(self, kls=None, **kls_kargs):
+        """ load_obj """
+        kls, kls_kargs = self.pre_load_obj(kls=kls, **kls_kargs)
+        obj = kls(**kls_kargs)
+        obj = self.post_load_obj(obj)
+        return obj
+
+    def post_load_obj(self, obj):
+        """ post_load_obj hook:
+              .. used for agent.play()
+        """
+        return obj
 
     def keys(self):
         """ dictionary compatability """
@@ -43,52 +106,98 @@ class Manager(object):
     def items(self):
         """ dictionary compatability """
         return self.registry.items()
+
     def __len__(self):
+        """ list/dictionary compatibility """
         return len(self.registry)
-    def __init__(self, *args, **kargs):
-        """ """
-        self.registry      = {}
-        self.generic_store = HierarchicalData() #generic storage for the actual manager itself.
 
     def __getattr__(self, name):
         """ by default attributes are lazy
         """
-        if name.startswith('_') or name == 'asset_class':
+        # Enforces privacy and special names
+        special_names = ['asset_class']
+        if name.startswith('_') or name in special_names:
             raise AttributeError, name
 
-        out = None
-
         try:
-                return object.__getattribute__(self, '__getitem__')(name)
+            return object.__getattribute__(self, '__getitem__')(name)
         except self.NotFound:
-                return getattr(object.__getattribute__(self, 'generic_store'), name)
+            return getattr(object.__getattribute__(self, 'generic_store'), name)
 
     @property
     def last(self):
-        """ all incoming data should be stamped; this function returns the oldest"""
+        """ All incoming assets should have been stamped;
+            this function returns the oldest asset. """
         return self[self.as_list[-1]]
 
     @property
     def first(self):
-        """ all incoming data should be stamped; this function returns the youngest"""
+        """ All incoming assets should have been stamped;
+            this function returns the youngest asset.
+        """
         return self[self.as_list[0]]
 
-    def stamp(self, name):
-        """ timestamp element with name <name> """
+    def _stamp(self, name):
+        """ Timestamps the asset with name <name>, safe
+            to call multiple times, but dishonesty is
+            discouraged.
+        """
         self.registry[name].stamp = datetime.datetime.now()
+
+    def pre_manage(self, name=None, kls=object, **kls_kargs):
+        """ pre_manage hook:
+              This function can modify the values, but must always
+              return the ....  Default is a no-op.
+        """
+        return name, kls, kls_kargs
+
+    def manage(self, name=None, kls=object, kls_kargs={}):
+        """ This function queues up a pile of future assets and the arguments
+            to initialize them with.  This pile will be dealt with when <load>
+            is called.
+        """
+        name, kls, kls_kargs = self.pre_manage(name=name, kls=kls, **kls_kargs)
+
+        self._pending.append([name, kls, kls_kargs])
+        return name
+
+    def post_manage(self):
+        """ pre_manage hook:
+
+              This function can modify the values, but must always
+              return the ....  Default is a no-op.
+        """
+        return
+
     def post_registration(self, asset):
-        """ """
-        NIY
+        """ post_registration hook:
+
+              This function can modify the value, but must always
+              return the new asset that is to be managed.  Default
+              is a no-op.
+        """
+        return asset
+
+    def pre_registration(self, name, **item_metadata):
+        """ pre_registration hook:
+
+              This function can modify the name, the item's metadata,
+              or both, but it must always return (name, item_metadata).
+              Default is a no-op.
+        """
+        return name, item_metadata
+
     def register(self, name, **item_metadata):
         """ register object <name> with namespace <item_metadata>
         """
+        name, item_metadata = self.pre_registration(name, **item_metadata)
         name = str(name)
-        self.registry[name] = getattr(self, 'asset_class', default_asset_class)()
+        self.registry[name] = getattr(self, 'asset_class', DEFAULT_ASSET_CLASS)()
         for key, value in item_metadata.items():
             setattr( self.registry[name], key, value)
-        self.stamp(name)
+        self._stamp(name)
         new_asset = self[name]
-        self.post_registration(new_asset)
+        new_asset = self.post_registration(new_asset)
         return new_asset
 
     def __str__(self):
@@ -97,7 +206,7 @@ class Manager(object):
 
     def __repr__(self):
         """ """
-        return 'manager(' + str(self) + ')'
+        return self.__class__.__name__ + '(' + str(self) + ')'
 
     def __getitem__(self, name):
         """ retrieve service by name
@@ -121,18 +230,21 @@ class Manager(object):
 
     @property
     def as_dict(self):
+        """ dictionary morphology """
         return dict(self.items())
 
     @property
     def as_list(self):
-        """ """
+        """ list morphology """
         out = [ x for x in self ]
         out.sort(lambda x, y: cmp(self.registry[x].stamp,
                                   self.registry[y].stamp))
         return out
 
-    aslist=as_list
+    aslist = as_list
 
     def __iter__(self):
-        """ dumb proxy """
+        """ list/dictionary compatibility: a dumb proxy """
         return iter(self.registry)
+
+DEFAULT_ASSET_CLASS = HierarchicalData
