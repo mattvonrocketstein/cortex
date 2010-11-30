e7b57ac5e922d45c595d3521b0065f5ff2dc04bb
mvr <matthewvonrocketstein@gmail-dot-com>
2010-11-30 00:47:26 -0500

    the terminal has a new way to attach input processors.

--- lib/cortex/services/terminal.py
+++ lib/cortex/services/terminal.py
@@ -27,15 +27,40 @@ from cortex.mixins import LocalQueue
 from cortex.util.decorators import constraint
 
 class ShellAspect:
+    """ Simple mixin to hold stuff that's pretty IPython specific.
+    """
+    registry = {}
+
     def set_prompt(self):
         """ """
         self.shell.IP.outputcache.prompt1.p_template = console.blue(self.universe.name) + ' [\\#] '
         self.shell.IP.outputcache.prompt2.p_template = console.red(self.universe.name)  + ' [\\#] '
-    def dispatch_input_processor(self, source):
-        """ """
-        pass
+
+    def attach_proc(self, func, predicate):
+        """ use func when predicate(source)
+        """
+        if not self.registry:
+            first_time = True
+
+        self.registry[func] = predicate
+        if first_time:
+            self.registry[self.get_input_processor()] = lambda source: "DEFAULT"
+            def new_method(source, IP, **kargs): #source, **kargs):
+                #raw_input(str(('new',self, args, kargs)))
+                #report('new',self, source,kargs)
+                for fxn, pred in self.registry.items():
+                    test = pred(source)
+                    if test and test != "DEFAULT":
+                        return fxn(source)
+                default_method = [[fxn, pred] for fxn,pred in self.registry.items() if pred('')=="DEFAULT"]
+                assert default_method, "No default method found, no attachments fit"
+                fxn, pred = default_method.pop()
+                return fxn(source)
+            self.replace_input_processor(new_method)
+        report("attached..")
 
     def get_input_processor(self):
+        """ """
         return self.shell.IP.runsource
 
     def replace_input_processor(self, new_proc):
