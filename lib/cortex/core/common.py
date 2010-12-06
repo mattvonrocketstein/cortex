0d5216d607d6ceadd6e8e242b1fa8a6b27b4ac1c
Matthew von Rocketstein <matt@telemachus.(none)>
2010-12-05 18:45:13 -0800

    added a new exception

--- lib/cortex/core/common.py
+++ lib/cortex/core/common.py
@@ -1,5 +1,8 @@
 """ cortex.core.common
 """
 
+class AgentPrerequisiteNotMet(Exception):
+    """ Move along, nothing to see here """
+
 class AgentError(AssertionError):
     """ nothing to see here, move along """
