f0dac0265178379497491c2fa5cfff9588e6540b
Matthew von Rocketstein <matt@telemachus.(none)>
2010-12-05 18:46:07 -0800

    moved scratch work out of this file

--- lib/cortex/store/ground.py
+++ lib/cortex/store/ground.py
@@ -14,21 +14,6 @@ from cortex.core.data import NOT_FOUND_T
 from cortex.core.hds import HierarchicalData
 from cortex.core.atoms import PersistenceMixin
 
-
-class HierarchicalWrapper(HierarchicalData):
-    """ """
-    def __init__(self, proxy_name):
-        super(HierarchicalWrapper,self).__init__()
-        self._proxy = proxy_name
-
-    def __getattr__(self, name):
-        """ """
-        if hasattr(self, '_proxy') and hasattr(self._proxy, name):
-            return getattr(object.__getattribute__(self,'_proxy'), name)
-        else:
-            return super(HierarchicalWrapper,self).__getattr__(name)
-
-
 class Memory(TSpace, PersistenceMixin):
     """ A thin wrapper around lindypy's tuplespace. """
 
