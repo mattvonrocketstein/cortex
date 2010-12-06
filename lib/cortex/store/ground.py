f523617bc571a6adefc190abc595562fc6548efe
Matthew von Rocketstein <matt@telemachus.(none)>
2010-12-05 21:39:45 -0800

    functional subspaces inside of keyspaces and accurate bookkeeping for them.

--- lib/cortex/store/ground.py
+++ lib/cortex/store/ground.py
@@ -13,82 +13,55 @@ from cortex.core.util import report
 from cortex.core.data import NOT_FOUND_T
 from cortex.core.hds import HierarchicalData
 from cortex.core.atoms import PersistenceMixin
+from cortex.store.tuplespace import CortexTSpace
+from cortex.store.mixins import TransformerMixin
 
-class Memory(TSpace, PersistenceMixin):
-    """ A thin wrapper around lindypy's tuplespace. """
+class Memory(CortexTSpace, PersistenceMixin, TransformerMixin):
+    """ The memory object can hopefully be used just like the data-store you need,
+        whatever it is. It morphs into a linda-style tuplespace / "shared blackboard",
+        a distributed hash table, or publish/subscribe style event bus.  It is
+        embeddable insofar as it supports explicitly named subspaces, allowing
+        everyone to see only the section they are interested in, or selectively
+        sync/update that subspace.
+    """
 
     universe = None
+    __init_tspace = TSpace.__init__
+
+    @property
+    def default_name(self):
+        """ """
+        return '{owner} :: {name}'.format(owner=str(self.owner), name=str(id(self.owner)))
+
+    def __init_memory(self):
+        """ """
+        self.john_hancock() # Sign it
 
     def __init__(self, owner, name=None, filename=None, parent_space=None):
         """ """
-        self.owner = owner
+        self.owner        = owner
         self.parent_space = parent_space
-        self.name  = name or (str(owner) + ' :: ' + str(id(owner)))
+        self.name         = name or self.default_name
 
-        # persistence mixin
-        if not filename:
-            universe = self.owner.universe
-            if universe:
-                filename = universe.tmpfile().name
-        else:
-            self.filename = filename
+        self.__init_persistence(filename)
+        self.__init_tspace()
+        self.__init_memory()
 
-        TSpace.__init__(self)
 
-        self.john_hancock() # Sign it
-        if parent_space is not None:
-            parent_space.install_subspace(self)
+        # request that parents install me as a subspace.
+        subspace_success = (parent_space is not None) and \
+                           (parent_space.install_subspace(self))
+        report("SubspaceCreation: ", subspace_success)
 
     def __repr__(self):
+        """ """
         return "<Memory@"+str(self.owner)+'>'
-
-    @property
-    def subspaces(self):
-        return self.filter(lambda tpl: tpl[0]=='__subspaces__')[0]
-
-    def install_subspace(self, other):
-        """ TODO: ensure type(self)==type(other)?
-            REFACTOR..
-        """
-
-        #sanity check
-        existential_test = lambda tpl: tpl[0] == other.name
-        search_results   = self.filter(existential_test)
-        err = "Already a subspace by the name of {name} in {space} !"
-        err = err.format(name=other.name, space=self.name)
-        assert not search_results, err
-
-        subspace = (other.name, other)
-        self.add(subspace)
-
-        # update subspace list
-        subspaces = self.subspaces + (other.name,)
-        self.get(self.subspaces, remove=True)
-        self.add(subspaces)
-
-        report('finished installing subspace')
 
     def john_hancock(self):
         """ Sign it. """
         self.add(('__name__',  self.name))
-        self.add(('__subspaces__',))
+        self.add(('__subspaces__',)) # required by subspace mixin
         self.add(('__stamp__', str(datetime.datetime.now())))
-
-    def as_keyspace(self, name=None):
-        """ TODO: use cortex.store.transform
-
-            NOTE: this is a cheap, stupid, and possibly error-prone
-                  cloning operation that is running.. but the alternative
-                  is very expensive
-        """
-        from cortex.store.keyspace import Keyspace
-        name = name or (self.name+':as:Keyspace')
-        k = type('dynamicKeyspace', (Keyspace,),{})(self, name=name)
-        k.__dict__ = self.__dict__ #copy.copy(self.__dict__)
-        return k
-
-    def asdf__getattr__(self,name):
-        print 'mem: getattr fail', name
 
     def shutdown(self):
         """ TODO: proxy to TSpace shutdown? what does it do? """
@@ -105,54 +78,11 @@ class Memory(TSpace, PersistenceMixin):
         PersistenceMixin.save(self.filename)
         report('persisted memory to', self.filename)
 
-    def get_many(self, pattern):
-        """ from lindypy """
-        out=[]
-        while True:
-            try:
-                out.append(self.get(pattern,remove=True))
-            except KeyError:
-                break
-        return out
-
-    def get(self, *args, **kargs):
-        """ from lindypy """
-        #print 'get', args, kargs
-        return TSpace.get(self, *args, **kargs)
-
-    def add(self, *args, **kargs):
-        """ from lindypy """
-        #print 'added', args, kargs
-        return TSpace.add(self, *args, **kargs)
-
-    #@cache these
-    def filter(self, *tests, **kargs):
-        """ new """
-        remove = kargs.pop('remove', False)
-        out    = tuple()
-        for item in iter(self.values(safe=True)):
-            #item =self.get(item)
-            passes = True
-            for test in tests:
-                if not test(item):
-                    passes = False
-            if passes:
-                out += (item,)
-                if remove:
-                    self.get(item, remove=remove)
-        return out
-
-    def values(self, safe=False):
-        """ from lindypy
-
-            NOTE: values can be stale.. safe ensures they are
-                  availible to be get'ed and not just ghosts
-        """
-        out = TSpace.values(self)
-        if safe:
-            for tpl in out:
-                try:
-                    self.get(tpl)
-                except KeyError:
-                    out.remove(tpl)
-        return out
+    def __init_persistence(self,filename):
+        """ persistence mixin """
+        if not filename:
+            universe = self.owner.universe
+            if universe:
+                filename = universe.tmpfile().name
+        else:
+            self.filename = filename
--- lib/cortex/store/keyspace.py
+++ lib/cortex/store/keyspace.py
@@ -5,9 +5,7 @@ from cortex.store.ground import Memory
 from cortex.store.transforms import DefaultKeyMapper
 
 class Keyspace(Memory, DefaultKeyMapper):
-    """ Thin wrapper around <Memory> to make it look like a dictionary
-    """
-    #def __init__(self, *
+    """ Thin wrapper around <Memory> to make it look like a dictionary """
 
     def __contains__(self,other):
         return other in self.keys()
@@ -19,22 +17,17 @@ class Keyspace(Memory, DefaultKeyMapper):
         return [ k for k in self.keys() if not any( map(k.startswith, FORBIDDEN_PREFIXES) ) ]
 
     def __getitem__(self, key):
-        """ TODO: is this tailored too much for the PostOffice, or is it sufficiently generic?
-            TODO: transparent encryption, serialization, perspective warping?
+        """ dictionary compatibility: (almost)
+
+              NOTE: this list may contain duplicates.. this is intentional
+              TODO: is this tailored too much for the PostOffice, or is it sufficiently generic?
+              TODO: transparent encryption, serialization, perspective warping?
         """
         matching_tuples = self.filter(lambda tpl: self.tuple2key(tpl)==key)
-        #assert len(matching_tuples)<2,"Found duplicate matching tuples.. not really a keyspace then, is it?"
         if matching_tuples:
             first_match = matching_tuples[0]
             return self.tuple2value(first_match)
         return NOT_FOUND_T
-
-    def subspace(self, name):
-        """ return a nested keyspace with name <name> """
-        NIY
-    def asdkeys(self):
-        if self.subspace_of:
-            return self.subspace_of[self.name]
 
     def __contains__(self,other):
         """ dictionary compatibility """
@@ -58,3 +51,11 @@ class Keyspace(Memory, DefaultKeyMapper):
         """
         return [ [ self.tuple2key(x), self.tuple2value(x) ] for x in self.values() ]
 
+class SingletonKeyspace(Keyspace):
+    """ Enforces stricter adherence to dictionary api.
+
+           (see keyspace.__getitem__ by constrast)
+    """
+    def __getitem__(self, key):
+        """ """
+        assert len(matching_tuples)<2,"Found duplicate matching tuples.."
