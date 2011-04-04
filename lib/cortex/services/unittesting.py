ddf43d6862c536d7ab7389b19819381dcf3a39ed
mvr <matthewvonrocketstein@gmail-dot-com>
2011-04-04 07:59:19 -0700

    [autonomy] service for helping with unittests

---
+++ lib/cortex/services/unittesting.py
@@ -0,0 +1,52 @@
+""" cortex.services.unittesting
+"""
+#import unittest2
+
+from unittest import TestCase
+
+from cortex.core.service import Service
+from cortex.core.atoms import Threadpooler
+from cortex.core.util import report, console
+
+class unittestservice(Threadpooler, Service):
+    """ """
+
+    def get_all_tests(self):
+        names = [x for x in dir(self.__class__) if x.startswith('test_')]
+        dct = dict([ [name, getattr(self, name)] for name in names ])
+        return dct
+
+    def iterate(self):
+        tests = self.get_all_tests().items()
+        report("Found {N} tests".format(N=len(tests)))
+        for name, test in tests:
+            report("testing",name)
+            yield test()
+        report("Finished Tests")
+
+class SanityCheck(unittestservice, TestCase):
+
+    def test_universe(self):
+        "is the universe setup? "
+        self.assertTrue(self.universe)
+
+    def test_reactor(self):
+        "is the reactor setup? "
+        self.assertTrue(self.universe.reactor)
+
+    def test_services(self):
+        " is the servicemanager a servicemanager? "
+        from cortex.core.service import ServiceManager
+        self.assertEqual(type(self.universe.services), ServiceManager)
+
+    def test_service_load_unloading(self):
+        """ """
+        class SimpleService(Service):
+            pass
+
+    def test_self_as_service(self):
+        " is the unittest service installed as a service? "
+        self.assertTrue( [ self.__class__.__name__.lower() in self.universe.services ] )
+
+    def test_reactor_calllaters(self):
+        pass
