8412470ca7a6b1a8ff00426649474a3714191ba3
mvr <matthewvonrocketstein@gmail-dot-com>
2011-04-06 23:03:06 -0700

    [mainline-no-traits] API testcase stub

--- lib/cortex/services/unittesting.py
+++ lib/cortex/services/unittesting.py
@@ -65,7 +65,9 @@ class ChannelCheck(TestCase):
 
 class UniverseCheck(TestCase):
     """ check various aspects of an engaged universe
-        for correctness """
+        for correctness. assumes universe is integrated
+        with twisted reactor
+    """
     def test_universe(self):
         "is the universe setup? "
         self.assertTrue(self.universe)
@@ -84,6 +86,16 @@ class UniverseCheck(TestCase):
         self.assertTrue('sanitycheck' in services)
         self.assertTrue('postoffice' in services)
         self.assertTrue('linda' in services)
+
+    def test_reactor_calls(self):
+        """ "self.universe.reactor.callLater(1, callback)"
+            does not work-- apparently threadpool blocks it.
+        """
+        class result_holder: switch=0
+        def callback(): result_holder.switch += 1
+        self.universe.reactor.callFromThread(callback)
+        import time;time.sleep(1)
+        self.assertEqual(result_holder.switch,1)
 
 class AgentCheck(TestCase):
     """ check various aspects of a running agent """
@@ -109,6 +121,20 @@ class AgentCheck(TestCase):
         """
         for service in self.services.values():
             self.test_autonomy2(service)
+from cortex.core.api import publish
+
+class APICheck(TestCase):
+    def test_api_publish(self):
+        contents = publish()
+        self.assertTrue('do' in contents)
+        self.assertTrue('declare_goals' in contents)
+        self.assertTrue('universe' in contents)
+
+    def test_api_instruction(self):
+        instruction([['load', 'service', {}]])
+
+    def test_do(self):
+        [['load', 'service', {}]]
 
 class TestTools:
     @property
@@ -128,15 +154,7 @@ class SanityCheck(UnitTestService, TestTools,AgentCheck, UniverseCheck, ChannelC
         " is the unittest service installed as a service? "
         self.assertTrue( [ self.__class__.__name__.lower() in self.universe.services ] )
 
-    def test_reactor_calls(self):
-        """ "self.universe.reactor.callLater(1, callback)"
-            does not work-- apparently threadpool blocks it.
-        """
-        class result_holder: switch=0
-        def callback(): result_holder.switch += 1
-        self.universe.reactor.callFromThread(callback)
-        import time;time.sleep(1)
-        self.assertEqual(result_holder.switch,1)
+
 
 
 UTS = UnitTestService
