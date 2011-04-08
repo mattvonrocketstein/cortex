4d04502c14aa96167cfae66b3637df1368604206
mvr <->
2011-04-07 21:27:04 -0700

    [tests] better tests harness

--- lib/cortex/services/unittesting.py
+++ lib/cortex/services/unittesting.py
@@ -7,11 +7,13 @@ from cortex.core.atoms import Threadpooler
 from cortex.core.util import report, console
 from cortex.core.metaclasses import META
 
-class UnitTestService(Threadpooler, Service, TestCase):
-    """ Cortex Service / Agent"""
+class UTS(Threadpooler, Service, TestCase):
+
 
     __metaclass__ = META
 
+class UnitTestService(UTS):
+    """ Cortex Service / Agent"""
     def get_all_tests(self):
         """ enumerate every test_* method in this instance's class """
         names = [x for x in dir(self.__class__) if x.startswith('test_')]
@@ -33,117 +35,14 @@ class UnitTestService(Threadpooler, Service, TestCase):
         test_suite.run(TestResult()) #unittest.main()
         yield
 
-class ChannelCheck(TestCase):
-    """ test the channel abstraction """
-    def test_channels(self):
-        class result_holder: switch=0
-        def callback(*args, **kargs): result_holder.switch = 1
-
-        # grab postoffice service handle from universe
-        poffice = (self.universe|'postoffice')
-
-        # ensure the events channel has already been registered with postoffice
-        chans = poffice.enumerate_embedded_channels()
-        chan_names = [ chan._label for chan in chans ]
-        self.assertTrue('EVENT_T' in chan_names)
-
-        # create a subchannel, make sure we can see it
-        chan_sandwich = poffice.event.sandwich
-        self.assertTrue(poffice.event.subchannels(),[chan_sandwich])
-
-        # test subscriptions
-        # (block for a second so callback gets hit)
-        chan_sandwich.subscribe(callback)
-        chan_sandwich("test")
-        import time; time.sleep(1)
-        if result_holder.switch == 0:
-            self.assertTrue(False and "callback not fired :(" )
-
-        # test that destroy unsubscribes and unregisters
-        chan_sandwich.destroy()
-        self.assertEqual(len(poffice.event.subchannels()), 0)
-
-class UniverseCheck(TestCase):
-    """ check various aspects of an engaged universe
-        for correctness. assumes universe is integrated
-        with twisted reactor
-    """
-    def test_universe(self):
-        "is the universe setup? "
-        self.assertTrue(self.universe)
-
-    def test_reactor(self):
-        "is the twisted reactor installed in the universe? "
-        self.assertTrue(self.universe.reactor)
-
-    def test_services(self):
-        " is the servicemanager a servicemanager? "
-        from cortex.core.service import ServiceManager
-        services = self.universe.services
-        self.assertEqual(type(services), ServiceManager)
-
-        services = self.services
-        self.assertTrue('sanitycheck' in services)
-        self.assertTrue('postoffice' in services)
-        self.assertTrue('linda' in services)
-
-    def test_reactor_calls(self):
-        """ "self.universe.reactor.callLater(1, callback)"
-            does not work-- apparently threadpool blocks it.
-        """
-        class result_holder: switch=0
-        def callback(): result_holder.switch += 1
-        self.universe.reactor.callFromThread(callback)
-        import time;time.sleep(1)
-        self.assertEqual(result_holder.switch,1)
-
-class AgentCheck(TestCase):
-    """ check various aspects of a running agent """
-    def test_autonomy(self):
-        "test basic autonomy: is the test is running, we should be started"
-        self.assertTrue(self.started)
-        self.assertTrue(not self.is_stopped)
-        self.assertTrue(not self.stopped)
-
-    def test_autonomy2(self,other=None):
-        """ test autonomy spec """
-        other = other or self
-        F = lambda x: callable(getattr(other,x,None) )
-        self.assertTrue(F('iterate'))
-        self.assertTrue(F('run'), "{o} has no run method".format(o=other))
-        self.assertTrue(F('start'))
-        self.assertTrue(F('stop'))
-
-    def test_autonomy_for_every_service(self):
-        """ every service registered with this universe
-            should use the autonomy mixin, and so should
-            implement the autonomy spec
-        """
-        for service in self.services.values():
-            self.test_autonomy2(service)
-from cortex.core.api import publish
-
-class APICheck(TestCase):
-    def test_api_publish(self):
-        contents = publish()
-        self.assertTrue('do' in contents)
-        self.assertTrue('declare_goals' in contents)
-        self.assertTrue('universe' in contents)
-
-    def test_api_instruction(self):
-        instruction([['load', 'service', {}]])
-
-    def test_do(self):
-        [['load', 'service', {}]]
-
-class TestTools:
     @property
     def services(self):
         """ proxy for convenience """
         return self.universe.services.as_dict
-
-class SanityCheck(UnitTestService, TestTools,AgentCheck, UniverseCheck, ChannelCheck):
-    _testMethodName = 'WhyDoesThisNeedToBeHere?'
+    def _post_init(self, bases):
+        """ """
+        report("Look ma, I rewrote my bases")
+        self.__class__.__bases__ += bases
 
     def test_service_load_unloading(self):
         """ """
@@ -156,5 +55,4 @@ class SanityCheck(UnitTestService, TestTools,AgentCheck, UniverseCheck, ChannelC
 
 
 
-
-UTS = UnitTestService
+    _testMethodName = 'WhyDoesThisNeedToBeHere?'
--- lib/cortex/tests/__main__.py
+++ lib/cortex/tests/__main__.py
@@ -1,30 +1,38 @@
-""" example boot script for cortex
+""" tests for cortex
+
+    turns out the best place to run tests are
+    to be inside the system that's being tested.
 """
 
-from cortex.core import api
-from cortex.core.node import Agent
-from cortex.core.service import Service
-from cortex.core.util import report, console
-from cortex.services.unittesting import SanityCheck
+from cortex.tests.channel import *
 
+
+
+from cortex.core import api
+from cortex.services.unittesting import UnitTestService
+import os, sys
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+from cortex.tests.channel import *
 
 ## Parameters for the services.. empty and ready to override
+bases = (AgentCheck, UniverseCheck, ChannelCheck)
 interactive = False                                  # Whether to run shell
 post_args   = {}                                     # Postoffice parameters
 term_args   = {}                                     # Cortex-Terminal arguments
 linda_args  = {}                                     # Linda (tuplespace) parameters
-test_args   = {}                                     # Parameters for unit-test service
+test_args   = dict(bases=bases)                      # Parameters for unit-test service
 
 ## Build a list of instructions recognizable to the cortex api.  Order does not matter
 ## here because the service definitions specify (and resolve) their own dependencies.
 instructions         = [[ "load_service", ("_linda",),         linda_args ],
                         [ "load_service", ("postoffice",),     post_args  ],
-                        [ "load_service", ("sanitycheck",),    test_args  ],]
+                        [ "load_service", ("unittestservice",),    test_args  ],]
 terminal_instruction = [[ "load_service", ("terminal",),       term_args  ],]
 if interactive: instructions += terminal_instruction
 
 # Specify terminating conditions to declare as goals
-tests_are_finished_running = lambda: (api.universe|'sanitycheck').stopped
+tests_are_finished_running = lambda: (api.universe|'UnitTestService').stopped
 
 # Declare goals, load services with the given parameters, and invoke mainloop
 api.declare_goals( [ tests_are_finished_running ] )
