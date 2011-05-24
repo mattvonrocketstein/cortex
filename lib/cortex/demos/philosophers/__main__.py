c35df3ddc5dbbb0d48568f1969e91a19bbadd41e
mvr <matthewvonrocketstein@gmail-dot-com>
2011-05-23 18:55:49 -0700

    moved harness.py to __main__ for philosophers demo

--- lib/cortex/demos/philosophers/__main__.py
+++ lib/cortex/demos/philosophers/__main__.py
@@ -5,7 +5,7 @@
 """
 from cortex.core.api import universe, declare_agent, load_instructions
 from cortex.demos.philosophers import Philosopher
-from cortex.instructions import LOAD_LINDA, LOAD_POST_OFFICE
+from cortex.instructions import LOAD_LINDA, LOAD_POST_OFFICE, LOAD_TERMINAL
 from cortex.core.util import Lock
 
 ## Build a list of instructions recognizable to the cortex api.  Order does not matter
