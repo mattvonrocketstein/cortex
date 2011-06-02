4c05f0db35301822b82f0f015bde00c362f22f20
mvr <matthewvonrocketstein@gmail-dot-com>
2011-06-02 00:46:28 -0400

    Revert "[philosophers] arithmetic operations on classes"
    This reverts commit 621e7788d02ea5aa9d0d32daaf9642e4498ccec9.

--- lib/cortex/mixins/mixin.py
+++ lib/cortex/mixins/mixin.py
@@ -1,8 +1,1 @@
-""" cortex/mixins/mixin """
-class Mixin:
-
-    @classmethod
-    def bind_to(self, other):
-        name = 'name(core)'.format(name=other.__name__,core=self.__name__)
-        bases = (other)
-        return type(name, bases, {})
+class Mixin: pass
