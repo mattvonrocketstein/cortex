7d4924bc94be104549447d7fd7f003056903fab9
mvr <->
2011-06-02 17:26:35 -0700

    [gui] template

---
+++ lib/cortex/services/gui.py
@@ -0,0 +1,45 @@
+""" cortex.services.gui
+      http://ipython.scipy.org/moin/Cookbook/EmbeddingInGTK
+"""
+
+from cortex.core import api
+from cortex.services import Service
+from cortex.core.data import EVENT_T
+from cortex.core.util import report, console
+from cortex.core.terminal import IPShellTwisted, IPY_ARGS
+from cortex.mixins import LocalQueue
+from cortex.util.decorators import constraint
+
+class GUI(Service):
+    """ """
+
+    import gtk
+    from ipython_view import *
+    import pango
+
+    import platform
+    if platform.system()=="Windows":
+        FONT = "Lucida Console 9"
+    else:
+        FONT = "Luxi Mono 10"
+
+    def start(self):
+        W = gtk.Window()
+        W.set_size_request(750,550)
+        W.set_resizable(True)
+        S = gtk.ScrolledWindow()
+        S.set_policy(gtk.POLICY_AUTOMATIC,gtk.POLICY_AUTOMATIC)
+        V = IPythonView()
+        V.modify_font(pango.FontDescription(FONT))
+        V.set_wrap_mode(gtk.WRAP_CHAR)
+        V.show()
+        S.add(V)
+        S.show()
+        W.add(S)
+        W.show()
+        W.connect('delete_event',lambda x,y:False)
+        W.connect('destroy',lambda x:gtk.main_quit())
+        gtk.main()
+
+if __name__=='__main__':
+    GUI().start()
