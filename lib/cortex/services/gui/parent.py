25578aa5e60ad885c0228bb7ea235786701c94d2
mvr <mvr>
2011-06-03 23:43:07 -0400

    [gui] refactor

--- lib/cortex/services/gui/parent.py
+++ lib/cortex/services/gui/parent.py
@@ -1,12 +1,18 @@
+""" cortex.services.gui.parent
+"""
 from ipython_view import *
 from common import CommonInterface
 from cortex.services.terminal import abstract
 from cortex.core.util import report, console
 
-
 class GUI(CommonInterface):
     """ """
     def set_shell(self):
+        """ get a gtk-embedded ipython thingy
+            this function might seem like it has a
+            weird name, but it's named to be similar
+            to the one in cortex.services.terminal.shell
+        """
         S = self.scrolled_window
         from cortex.core.data import IPY_ARGS
         self.shell = IPythonView(argv=IPY_ARGS,
@@ -19,7 +25,8 @@ class GUI(CommonInterface):
 
     def really_start(self):
         """ TODO: defer to universe.command_line_options
-                  for whether to magic_pdb """
+                  for whether to magic_pdb
+        """
         if not self.universe.config.gtk_reactor==True:
             err  = "This universe isn't configured for GTK reactor, "
             err += "but you're trying to use the gtk terminal!"
@@ -27,30 +34,29 @@ class GUI(CommonInterface):
             self.fault(err, ctx)
             self.universe.stop()
 
-        components  = [ self.spawn_channel_watcher,
-                        self.spawn_shell, ]
+
+        from cortex.services.gui.channel_window import ChannelAgent
+        from cortex.services.gui.shell import Shell
+
+        components  = [
+            dict(kls=ChannelAgent,
+                 kls_kargs=dict(universe=self.universe),
+                 name='ChannelAgent'),
+            dict(kls=Shell,
+                kls_kargs=dict(universe=self.universe),
+                 name='ShellAgent')
+            ]
 
         for c in components:
-            c()
+            self.manage(**c)
         self.load()
 
-    def spawn_channel_watcher(self):
-        from cortex.services.gui.channel_window import ChannelAgent
-        self.manage(kls=ChannelAgent, kls_kargs=dict(universe=self.universe),
-                    name='ChannelAgent')
 
     def spawn_shell(self):
         """ interesting.. safe to call multiple times"""
         report('spawns')
-        self.manage(kls=Shell, kls_kargs=dict(universe=self.universe), name='ShellAgent')
+
 
 from cortex.core.agent import Agent
 class Window(Agent, GUI): pass
 GUIChild = Window
-
-class Shell(Window):
-    def start(self):
-        window = self.spawn_window
-        window.add(self.set_shell())
-        window.show()
-        self.set_prompt()
