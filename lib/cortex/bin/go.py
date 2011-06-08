544bb9dcd2e69924009efe7a4b9d5216defb906f
mvr <matthewvonrocketstein@gmail-dot-com>
2011-06-07 21:42:20 -0400

    [master] merged sessions in with test changes

--- lib/cortex/bin/go.py
+++ lib/cortex/bin/go.py
@@ -43,9 +43,8 @@ def entry():
     options, args = parser.parse_args()
     nodeconf_file = ABS_NODE_CONF or options.conf or REL_NODE_CONF
 
-    #  python interpretter compatability, coverage usage like:
-    #   shell$ cortex -c"import cortex; print cortex.__file__"
-    #   shell$ cortex somefile
+    #  python interpretter compatability:
+    #    shell$ cortex -c"import cortex; print cortex.__file__"
     if args and len(args)==1:
         fname = args[0]
         if os.path.exists(fname):
@@ -53,15 +52,19 @@ def entry():
             __file__ = os.path.abspath(fname)
             execfile(fname)
             return
+
+    #  python interpretter compatability:
+    #    shell$ shell$ cortex somefile
     elif options.command:
         exec(options.command)
         return
 
-    # use the gtk-reactor?
+
     if options.run_tests:
         from cortex.tests import main
         return main()
 
+    # use the gtk-reactor?
     if options.gtk_reactor:
         print "using gtk reactor"
         from twisted.internet import gtk2reactor # for gtk-2.0
@@ -76,24 +79,26 @@ def entry():
     from cortex.core.reloading_helpers import run as RUN
     Universe.directives = options.directives.split(",")
 
-    # mirror command-line options in universe's config
+    # reflect command-line options in universe's config
     olist = [x for x in dir(options) if not x.startswith('_') and x not in 'read_file read_module ensure_value'.split()]
     [setattr(Universe.config,x,getattr(options,x)) for x in olist]
 
     ## deserialize a saved universe and resume it
     if options.universe:
-        if not os.path.exists(options.universe):
-            print "Path does not exist."
-            sys.exit()
-        else:
-            U = pickle.loads(open(options.universe).read())
-            U.play() # Invoke the Universe
+        verify_file(options.universe)
+        U = pickle.loads(open(options.universe).read())
+        U.play() # Invoke the Universe
         return
 
     # Otherwise install the nodeconf that we found and run it.
     install_nodeconf(nodeconf_file, options, args)
     RUN() # Invoke the Universe
     s=Universe.play()
+
+def verify_file(f):
+    if not os.path.exists(f):
+        print "Path does not exist."
+        sys.exit()
 
 if __name__ == '__main__':
     entry()
