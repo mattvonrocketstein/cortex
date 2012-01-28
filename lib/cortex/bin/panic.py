f6754df086ff5ae93bebf33d0f526aa20b6fa806
mvr <matthewvonrocketstein@gmail-dot-com>
2012-01-28 06:25:07 -0500

    better panic script, removing busted autoreload functionality, updating gitignore

--- lib/cortex/bin/panic.py
+++ lib/cortex/bin/panic.py
@@ -3,7 +3,8 @@ import os
 
 def entry():
     print '-'*80
-    _procs = "ps aux|grep matt|grep python|grep -v stat|grep -v grep|grep -v pbat|grep -v wic"
+    _procs = "ps aux|grep {user}|grep python|grep cortex"
+    _procs = _procs.format(user=os.environ['USER'])
     procs = os.popen(_procs).read()
     print procs
     print '-'*80
@@ -11,4 +12,3 @@ def entry():
     print pids
     _pids = ' '.join(pids)
     print os.popen('kill -KILL '+_pids).read()
-
