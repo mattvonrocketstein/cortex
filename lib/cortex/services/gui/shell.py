96217f2f4ac0d4925098ce54541eccfa579ee950
mvr <matthewvonrocketstein@gmail-dot-com>
2012-01-28 08:08:02 -0500

    adding a way to modify the gui shell's namespace

--- lib/cortex/services/gui/shell.py
+++ lib/cortex/services/gui/shell.py
@@ -1,3 +1,12 @@
+""" cortex.services.gui.shell
+
+    since there's not much here it might seem fragmented to have
+    an agent for this rather than just putting this in the GUI
+    service directly, but it makes sense if you think about the
+    possibility of the gui having menus, and split panes, and
+    scrolling log buffers, etc etc
+
+"""
 from cortex.services.gui.parent import Window
 class Shell(Window):
     def start(self):
