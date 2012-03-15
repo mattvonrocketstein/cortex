409cd44a7cec97920bd3bd20bb08ba918171aa6e
mvr <matthewvonrocketstein@gmail-dot-com>
2012-03-15 14:01:27 -0400

    dispatch based on type to determine template

--- lib/cortex/services/web/template.py
+++ lib/cortex/services/web/template.py
@@ -1,6 +1,10 @@
 """
 """
+from jinja2.ext import with_
 from jinja2 import Environment, PackageLoader
-env = Environment(loader=PackageLoader('cortex.services.web', 'templates'))
+from cortex.contrib.pygments_extension import PygmentsExtension
+env = Environment(loader=PackageLoader('cortex.services.web', 'templates'),
+                  extensions=[PygmentsExtension,with_])
+
 def template(name):
     return env.get_template(name + '.html')
