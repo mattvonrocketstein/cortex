65349085f9d2f86ac8e969cf6ef1e979d516bca9
mvr <matthewvonrocketstein@gmail-dot-com>
2012-03-15 13:01:22 -0400

    iterating

--- lib/cortex/services/web/eventdemo/alerts.py
+++ lib/cortex/services/web/eventdemo/alerts.py
@@ -1,26 +1,36 @@
-from nevow import athena,loaders, tags as T, inevow, rend
+"""
+"""
+
+from nevow import athena, loaders, tags as T, inevow, rend
 from twisted.python import filepath,util
-from events import EventHandler
+
+from cortex.core.util import report
+
+from .events import EventHandler
+
+_tpl = filepath.FilePath(__file__).parent().child('templates').child('alerts_table.html')
+modulePath = filepath.FilePath(__file__).parent().child('js').child('alerts.js')
 
 class AlertElement(athena.LiveElement):
     jsClass = u'Alerts.AlertWidget'
-    _tpl = filepath.FilePath(__file__).parent().child('templates').child('alerts_table.html')
-    docFactory = loaders.xmlfile(_tpl.path)
+
+    @property
+    def docFactory(self):
+        return loaders.xmlfile(_tpl.path)
 
     def __init__(self, eventHandler, *a, **kw):
         super(AlertElement, self).__init__(*a, **kw)
         eventHandler.addSubscriber(self)
 
     def fireEvent(self, eventID):
+        report('yo', eventID)
         self.callRemote('addRow', unicode(eventID))
 
 class AlertPage(athena.LivePage):
     def __init__(self, eventHandler, *a, **kw):
         super(AlertPage, self).__init__(*a,**kw)
         self.eventHandler = eventHandler
-        modulePath = filepath.FilePath(__file__).parent().child('js').child('alerts.js')
-        self.jsModules.mapping.update({
-            'Alerts': modulePath.path})
+        self.jsModules.mapping.update({'Alerts': modulePath.path})
 
     docFactory = loaders.stan(T.html[
         T.head(render=T.directive('liveglue')),
