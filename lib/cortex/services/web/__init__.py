ebeb512cf35caad4d04748249ca6acce29207599
mvr <matthewvonrocketstein@gmail-dot-com>
2012-03-18 19:29:22 -0400

    ajaxy nav

--- lib/cortex/services/web/__init__.py
+++ lib/cortex/services/web/__init__.py
@@ -14,7 +14,7 @@ from cortex.core.util import report
 from cortex.services import Service
 from cortex.core.data import EVENT_T
 from cortex.util.decorators import constraint
-from cortex.services.web.resource import Root, ObjResource
+from cortex.services.web.resource import Root, ObjResource, NavResource
 
 from cortex.services.web.eventdemo import rootpage
 from cortex.mixins import LocalQueue
@@ -49,13 +49,15 @@ class Web(LocalQueue, Service):
         favicon    = os.path.join(static_dir, 'favicon.ico')
 
         root = Root()
+
+        site = server.Site(root)
+
         root.putChild('static',      static.File(static_dir))
         root.putChild('favicon.ico', static.File(favicon))
         root.putChild('web',         ObjResource(self))
         root.putChild('universe',    ObjResource(self.universe))
         root.putChild("_code",       static.File(code_dir))
-
-        site = server.Site(root)
+        root.putChild('main_nav',      NavResource(root))
         self.universe.reactor.listenTCP(1338, site)
 
     def start_event_hub(self):
@@ -73,16 +75,14 @@ class Web(LocalQueue, Service):
         """ """
         report('iterating')
         return
-        e = self.pop_q()
+        """e = self.pop_q()
         if not e:
             return
         args, kargs = e
         report('iterating',e)
         import urllib,os
-        cmd = """wget --post-data "{0}" http://127.0.0.1:1339/event/foo/blue -o -"""
+        cmd = 'wget --post-data "{0}" http://127.0.0.1:1339/event/foo/blue -o -'
         cmd = cmd.format(urllib.urlencode(dict(data=self.pop_q())))
-        os.system('cd /tmp; '+cmd)
-Web = ThreadedIterator.from_class(Web)
+        os.system('cd /tmp; '+cmd)"""
 
-#if __name__=='__main__':
-#    print ThreadedIterator.from_class(Web)
+Web = ThreadedIterator.from_class(Web)
--- lib/cortex/services/web/resource/root.py
+++ lib/cortex/services/web/resource/root.py
@@ -14,3 +14,26 @@ class Root(Resource):
         ctx = dict(children=children,
                    contents=dir(self))
         return str(template('root').render(**ctx))
+class NavResource(Resource):
+
+    isLeaf = True
+    def __init__(self, obj):
+        self._obj = obj
+
+    def getChild(self, name, request):
+        if name == '': return self
+        return Resource.getChild(self, name, request)
+
+    def __init__(self,root=None):
+        self.root = root
+
+    @property
+    def _children(self):
+        children = self.root.children.copy()
+        children.pop('static','')
+        children.pop('favicon.ico','')
+        return children
+
+    def render_GET(self,request):
+        ctx = dict(children=self._children,)
+        return str(template('nav').render(**ctx))
