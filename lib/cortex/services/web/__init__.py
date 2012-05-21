d141e47ea226d4f8caa26e46a6ea6093f9ae8856
mvr <matthewvonrocketstein@gmail-dot-com>
2012-05-21 19:03:21 -0400

    define FecundService

--- lib/cortex/services/web/__init__.py
+++ lib/cortex/services/web/__init__.py
@@ -1,6 +1,7 @@
 """ cortex.services.web
 """
 import os
+import copy
 import urllib
 
 from nevow import appserver
@@ -22,30 +23,54 @@ from cortex.services.web.util import draw_ugraph, ugraph
 
 from .eventhub import EventHub
 
-class Web(Service, AgentManager):
-    """ Web Service:
-        start: start main webserver, and secondary event-hub
-        stop:  brief description of shutdown here
+class FecundService(Service, AgentManager):
+    """ FecundService describes a service with children.
+
+        You get mostly the semantics you'd expect.  When the
+        parent is start()'ed, the children start, and similarly
+        for stop().
+
+        To use, subclassers should define a Meta like this:
+
+
+           class Meta:
+               children = [ChildClass-1, ChildClass-2, .. ]
     """
+
+    class Meta:
+        abstract = True
 
     def __init__(self, *args, **kargs):
         Service.__init__(self, **kargs)
         AgentManager.__init__(self, **kargs)
 
-    @constraint(boot_first='postoffice')
     def start(self):
-        """ TODO: move these children to Meta.children=[] .. """
+        """ """
         ctx = dict(universe=self.universe)
-        for kls in [EventHub, WebRoot]:
-            self.manage(kls=kls, kls_kargs=ctx,
-                        name=kls.__name__)
+        child_classes = self.__class__.Meta.children
+        for kls in child_classes:
+            name = kls.__name__
+            kargs = dict(kls=kls, kls_kargs=ctx, name=name)
+            self.manage(**kargs)
         self.load()
         Service.start(self)
 
+
 class WebRoot(Agent):
-    """ TODO: smarter if you can't import networkx et al '"""
+    """ TODO: act smarter if you can't import networkx et al '"""
 
     def iterate(self):
+        """ WebRoot is a trivial Agent with no  true concurrency
+            flavor.  this iterate method will run once, and runs
+            only after the system is otherwise bootstrapped.  we
+            call the draw_ugraph using multiprocessing, because
+            something about the implementation of matplotlib/nx
+            or whatever else wants the main thread.  doing this
+            instead with callInThread, callFromThread, etc, have
+            all been tried, and although they might even seem to
+            work initially it seems to also leak memory or
+            something
+        """
         self.graph_f = self.universe.tmpfname(suffix='png')
         self.root.putChild('ugraph.png', File(self.graph_f))
         self.universe.callInProcess(draw_ugraph,
@@ -54,8 +79,8 @@ class WebRoot(Agent):
                                              self.graph_f, report ) )
 
     def stop(self):
-        """ TODO: stop doesn't stop anything except the eventhub..
-            make for to turn off the webs also
+        """ TODO: stop doesn't stop anything except the
+                   eventhub it to turn off the webs also
 
             http://mumak.net/stuff/twisted-disconnect.html
         """
@@ -68,6 +93,9 @@ class WebRoot(Agent):
                 report("Ignoring error: "+str(e))
 
     def setup(self):
+        """ setup for several things that can be
+            easily handled outside of the main loop.
+        """
         d          = os.path.dirname(__file__)
         static_dir = os.path.join(d, 'static')
         favicon    = os.path.join(static_dir, 'favicon.ico')
@@ -77,3 +105,14 @@ class WebRoot(Agent):
         self.root.putChild("_code",       static.File(os.path.dirname(cortex.__file__)))
         site       = server.Site(self.root)
         self.listener = self.universe.listenTCP(1338, site)
+
+class Web(FecundService):
+    """ Web Service:
+        start: start main webserver, and secondary event-hub
+        stop:  brief description of shutdown here
+    """
+    class Meta:
+        children = [EventHub, WebRoot]
+
+    @constraint(boot_first='postoffice')
+    def start(self): super(Web, self).start()
