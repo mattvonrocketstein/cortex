0f6c7e58dc3c6f6bf4adab3abde7917fe1447f42
mvr <mvr>
2012-05-22 19:55:34 -0400

    correct dox

--- lib/cortex/mixins/local_queue.py
+++ lib/cortex/mixins/local_queue.py
@@ -5,9 +5,8 @@ import Queue
 from Queue import Empty as QueueEmpty
 
 class LocalQueue:
-    """ currently only used by the terminal service?
-
-        TODO: parametrize 'event_q' name """
+    """ TODO: parametrize 'event_q' name
+    """
     def __len__(self):
         return self.event_q.qsize()
 
