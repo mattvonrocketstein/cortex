eead30dc61ebf1b7095005e13882e70201434a55
mvr <mvr>
2012-05-24 11:29:08 -0400

    queue cleaner works better

--- lib/cortex/services/dojo.py
+++ lib/cortex/services/dojo.py
@@ -10,14 +10,10 @@ from cortex.core.util import report
 from cortex.mixins.flavors import LoopingCall
 
 class DojoProcess(Process):
-    #_cmd = './node/bin/dojo --conf ~/code/robotninja/dojo.ini'
-    _cmd = 'yes dammit'
+    _cmd = './node/bin/dojo --conf ~/code/robotninja/dojo.ini'
+    #_cmd = 'python -c"for x in range(10): print x" '
 
-class Janitor(Agent):
-    """ make sure that the queue in DojoProcess
-        tracking stdout doesn't get too large.
-    """
-
+class QueueMon(Agent):
     max_size = 200
 
     def setup(self):
@@ -26,23 +22,44 @@ class Janitor(Agent):
         def newiterate(): lc.start(5)
         self.iterate = newiterate
 
-    @property
-    def queues(self):
-        djp = self.siblings['DojoProcess'] # self.parent.children()[0]
-        stdout = djp.stdout
-        stderr = djp.stderr
-        return stdout, stderr
+    def clean_queue(self, q, name="anonymous"):
+        """ clean_queue simply downsizes a queue by
+            throwing away the entries if qsize > max_size
+        """
+        if q.qsize() > self.max_size:
+            report("cleaning queue",name)
+            while q.qsize() > self.max_size:
+                q.get()
 
     def iterate(self):
-        stdout,stderr = self.queues
-        def clean(q,name):
-            if q.qsize() > self.max_size:
-                report("cleaning queue",name)
-                while q.qsize() > self.max_size:
-                    q.get()
-        clean(stdout, 'stdout')
-        clean(stderr, 'stderr')
+        """ self.queues should return be thing like a dict, a
+            dict.items(), or a flat list.
+        """
+        qs = self.queues
+        if qs:
+            if isinstance(qs, dict):
+                qs = qs.items()
+            elif all([isinstance(qs, (list,tuple)),
+                      not isinstance(qs[0],(list, tuple))]):
+                qs = [['anonymous',q] for q in qs]
+        else:
+            report('"queues" is empty or false?')
+            return
+        for name,q in qs:
+            self.clean_queue(q, name)
 
+class Janitor(QueueMon):
+    """ make sure that the queue in DojoProcess
+        tracking stdout doesn't get too large.
+    """
+    max_size = 200
+
+    @property
+    def queues(self):
+        djp = self.siblings['DojoProcess']
+        stdout = djp._stdout
+        stderr = djp._stderr
+        return dict(stdout=stdout, stderr=stderr)
 
 class Dojo(FecundService):
 
