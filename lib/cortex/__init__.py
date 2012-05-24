782ec824271b4759755193f37b2df7ee896ea822
mvr <mvr>
2012-05-24 19:27:58 -0400

    add some references

--- lib/cortex/__init__.py
+++ lib/cortex/__init__.py
@@ -1,2 +1,10 @@
 """ cortex
+
+    Inspired by:
+
+      Principles of Agent Oriented Programming [ Batista, Marietto, et al. (UFABC, Brazil) ]
+        http://cdn.intechopen.com/pdfs/14520/InTech-Principles_of_agent_oriented_programming.pdf
+
+      Agent Oriented Programming [ Shoham ]
+        http://www.cs.unb.ca/~ulieru/Teaching/CS6705/Shoham.pdf
 """
