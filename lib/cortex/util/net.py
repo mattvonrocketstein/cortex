9f43549ebb47c1c0f302082ab8b7f146cfe298a4
mvr <mvr>
2012-05-29 18:56:51 -0400

    add is_open() method for port@host

--- lib/cortex/util/net.py
+++ lib/cortex/util/net.py
@@ -3,62 +3,20 @@
 
 import socket
 
-def win_ipaddr():
-    """ stolen from stackoverflow.. this might be
-        useful for windows one of these days
+from cortex.core.data import LOOPBACK_HOST
+
+def is_open(port, ip=LOOPBACK_HOST):
+    """ answers whether port@ip is up or down.
+        this probably doesn't work everywhere..
+        but would you prefer i use nmap?
     """
-    from ctypes import Structure, windll, sizeof
-    from ctypes import POINTER, byref
-    from ctypes import c_ulong, c_uint, c_ubyte, c_char
-    MAX_ADAPTER_DESCRIPTION_LENGTH = 128
-    MAX_ADAPTER_NAME_LENGTH = 256
-    MAX_ADAPTER_ADDRESS_LENGTH = 8
-    class IP_ADDR_STRING(Structure):
-        pass
-    LP_IP_ADDR_STRING = POINTER(IP_ADDR_STRING)
-    IP_ADDR_STRING._fields_ = [
-        ("next", LP_IP_ADDR_STRING),
-        ("ipAddress", c_char * 16),
-        ("ipMask", c_char * 16),
-        ("context", c_ulong)]
-    class IP_ADAPTER_INFO (Structure):
-        pass
-    LP_IP_ADAPTER_INFO = POINTER(IP_ADAPTER_INFO)
-    IP_ADAPTER_INFO._fields_ = [
-        ("next", LP_IP_ADAPTER_INFO),
-        ("comboIndex", c_ulong),
-        ("adapterName", c_char * (MAX_ADAPTER_NAME_LENGTH + 4)),
-        ("description", c_char * (MAX_ADAPTER_DESCRIPTION_LENGTH + 4)),
-        ("addressLength", c_uint),
-        ("address", c_ubyte * MAX_ADAPTER_ADDRESS_LENGTH),
-        ("index", c_ulong),
-        ("type", c_uint),
-        ("dhcpEnabled", c_uint),
-        ("currentIpAddress", LP_IP_ADDR_STRING),
-        ("ipAddressList", IP_ADDR_STRING),
-        ("gatewayList", IP_ADDR_STRING),
-        ("dhcpServer", IP_ADDR_STRING),
-        ("haveWins", c_uint),
-        ("primaryWinsServer", IP_ADDR_STRING),
-        ("secondaryWinsServer", IP_ADDR_STRING),
-        ("leaseObtained", c_ulong),
-        ("leaseExpires", c_ulong)]
-    GetAdaptersInfo = windll.iphlpapi.GetAdaptersInfo
-    GetAdaptersInfo.restype = c_ulong
-    GetAdaptersInfo.argtypes = [LP_IP_ADAPTER_INFO, POINTER(c_ulong)]
-    adapterList = (IP_ADAPTER_INFO * 10)()
-    buflen = c_ulong(sizeof(adapterList))
-    rc = GetAdaptersInfo(byref(adapterList[0]), byref(buflen))
-    if rc == 0:
-        for a in adapterList:
-            adNode = a.ipAddressList
-            while True:
-                ipAddr = adNode.ipAddress
-                if ipAddr:
-                    yield ipAddr
-                adNode = adNode.next
-                if not adNode:
-                    break
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect((ip, int(port)))
+        s.shutdown(2)
+        return True
+    except:
+        return False
 
 def ipaddr_with_internet():
     """ you need to make an outbound connection to get
