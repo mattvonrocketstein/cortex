7ca13d544e73d2c2dc8f84bc53cd8afa443c381c
mvr <mvr>
2012-06-02 14:01:50 -0400

    simplify resources with baseclass

--- lib/cortex/services/web/resource/root.py
+++ lib/cortex/services/web/resource/root.py
@@ -5,46 +5,40 @@ from twisted.web.resource import Resource
 
 from twisted.web import static as _static
 from cortex.services.web.template import template
-
-class Root(Resource):
-    def __init__(self,favicon=None, static=None):
-        Resource.__init__(self)
-        self.putChild('static',      _static.File(static))
-        self.putChild('favicon.ico', _static.File(favicon))
-        self.putChild('main_nav',     NavResource(self))
-
-    def getChild(self, name, request):
-        if name == '': return self
-        return Resource.getChild(self, name, request)
+class CBR(Resource):
+    " Cortex base-resource "
 
     def render_GET(self, request):
         """ """
-        children = self.children.copy()
-        children.pop('static','')
-        children.pop('favicon.ico','')
-        ctx = dict(children=children,
-                   contents=dir(self))
-        return str(template('root').render(**ctx))
-class NavResource(Resource):
-
-    isLeaf = True
-    def __init__(self, obj):
-        self._obj = obj
+        T, ctx = self.get_template_ctx(request)
+        return str(template(T).render(**ctx))
 
     def getChild(self, name, request):
         if name == '': return self
         return Resource.getChild(self, name, request)
 
-    def __init__(self,root=None):
-        self.root = root
+class Root(CBR):
+    def __init__(self, favicon=None, static=None):
+        Resource.__init__(self)
+        self.putChild('static',      _static.File(static))
+        self.putChild('favicon.ico', _static.File(favicon))
+        self.putChild('main_nav',     NavResource())
+
+
+    def get_template_ctx(self, request):
+        """ """
+        children = self.children.copy()
+        return ('root',
+                dict(children=children,
+                     contents=dir(self)))
+
+class NavResource(CBR):
+
+    isLeaf = True
 
     @property
     def _children(self):
-        children = self.root.children.copy()
-        children.pop('static','')
-        children.pop('favicon.ico','')
-        return children
+        return 'universe _code web'.split()
 
-    def render_GET(self,request):
-        ctx = dict(children=self._children,)
-        return str(template('nav').render(**ctx))
+    def get_template_ctx(self, request):
+        return ('nav', dict(children=self._children,))
