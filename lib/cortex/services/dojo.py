5fb7239401d0d41390b5e0fbf64ccab280b852c1
mvr <mvr>
2012-06-17 02:57:06 -0400

    abstracting qmon

--- lib/cortex/services/dojo.py
+++ lib/cortex/services/dojo.py
@@ -3,55 +3,25 @@
     TODO: milestone.. how best to externalize this
 """
 
-from cortex.services import FecundService
-from cortex.agents.proc import Process
 from cortex.core.agent import Agent
 from cortex.core.util import report
-from cortex.mixins.flavors import LoopingCall
+from cortex.services import FecundService
+from cortex.agents.proc import Process
+from cortex.agents.qmon import QueueMon
+
+class Pile(Agent):
+    """ pile of stuff """
+    def iterate(self):
+        report('pile here')
 
 class DojoProcess(Process):
     _cmd = './node/bin/dojo --conf ~/code/robotninja/dojo.ini'
-    #_cmd = 'python -c"for x in range(10): print x" '
-
-class QueueMon(Agent):
-    max_size = 200
-
-    def setup(self):
-        olditerate = self.iterate
-        lc = LoopingCall(olditerate)
-        def newiterate(): lc.start(5)
-        self.iterate = newiterate
-
-    def clean_queue(self, q, name="anonymous"):
-        """ clean_queue simply downsizes a queue by
-            throwing away the entries if qsize > max_size
-        """
-        if q.qsize() > self.max_size:
-            report("cleaning queue",name)
-            while q.qsize() > self.max_size:
-                q.get()
-
-    def iterate(self):
-        """ self.queues should return be thing like a dict, a
-            dict.items(), or a flat list.
-        """
-        qs = self.queues
-        if qs:
-            if isinstance(qs, dict):
-                qs = qs.items()
-            elif all([isinstance(qs, (list,tuple)),
-                      not isinstance(qs[0],(list, tuple))]):
-                qs = [['anonymous',q] for q in qs]
-        else:
-            report('"queues" is empty or false?')
-            return
-        for name,q in qs:
-            self.clean_queue(q, name)
 
 class Janitor(QueueMon):
     """ make sure that the queue in DojoProcess
         tracking stdout doesn't get too large.
     """
+
     max_size = 200
 
     @property
@@ -64,4 +34,4 @@ class Janitor(QueueMon):
 class Dojo(FecundService):
 
     class Meta:
-        children = [ DojoProcess, Janitor ]
+        children = [ DojoProcess, Janitor, Pile ]
