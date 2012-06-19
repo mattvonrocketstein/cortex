58fd468d473a50875273f2280835092928df6390
mvr <mvr>
2012-06-19 17:56:19 -0400

    minor change

--- lib/cortex/agents/proc.py
+++ lib/cortex/agents/proc.py
@@ -9,7 +9,8 @@ from twisted.internet import protocol
 from cortex.core.util import report
 from cortex.core.agent import Agent
 
-def  q2txt(q):
+def q2txt(q):
+    """ """
     out = StringIO()
     while True:
         try:
@@ -28,30 +29,37 @@ class Process(Agent, protocol.ProcessProtocol):
     """
 
     def connectionMade(self):
+        """ """
         self.transport.closeStdin()
 
     def outReceived(self, data):
-        #report( "outReceived! with %d bytes!" % len(data))
+        """ """ #report( "outReceived! with %d bytes!" % len(data))
         self._stdout.put(data)
 
     def errReceived(self, data):
-        #report( "errReceived! with %d bytes!" % len(data))
+        """ """ #report( "errReceived! with %d bytes!" % len(data))
         self._stderr.put(data)
 
     def inConnectionLost(self):
+        """ """
         pass #report( "inConnectionLost! stdin is closed! (we probably did it)")
 
     def outConnectionLost(self):
+        """ """
         report("outConnectionLost! The child closed their stdout! queue-length: ",
                self._stdout.qsize())
 
     def errConnectionLost(self):
+        """ """
         report('errConnectionLost! The child closed their stderr. queue-length: ',
                self._stderr.qsize())
 
-    def processExited(self, reason): self.finish(reason)
+    def processExited(self, reason):
+        """ """
+        self.finish(reason)
 
     def finish(self, reason):
+        """ """
         err = reason.value.exitCode
         report( "processExited, status", err)
         if err:
@@ -65,7 +73,6 @@ class Process(Agent, protocol.ProcessProtocol):
         """ exhausts the queue"""
         return q2txt(self._stdout)
 
-
     @property
     def stderr(self):
         """ exhausts the queue"""
@@ -76,6 +83,7 @@ class Process(Agent, protocol.ProcessProtocol):
         self.transport.signalProcess('KILL')
 
     def stop(self):
+        """ """
         report('Killing process: ', self._cmd)
         try:
             self._kill()
@@ -84,6 +92,7 @@ class Process(Agent, protocol.ProcessProtocol):
         super(Process, self).stop()
 
     def setup(self):
+        """ """
         self._stdout = Queue()
         self._stderr = Queue()
         self.count   = 0
@@ -91,6 +100,7 @@ class Process(Agent, protocol.ProcessProtocol):
 
     @property
     def cmd(self):
+        """ """
         cmd = self._cmd
         cmd = shlex.split(cmd)
         args = cmd[1:] or [''] # doesn't like "null argv"? wtf
@@ -98,13 +108,18 @@ class Process(Agent, protocol.ProcessProtocol):
         return executable, args
 
     def iterate(self):
+        """ """
         env = {}
         executable, args = self.cmd
-        self.process = self.universe.reactor.spawnProcess(self, executable, args, env)
+        sp_args = (self, executable, args, env)
+        self.process = self.universe.reactor.spawnProcess(*sp_args)
 
     def start(self):
+        """ """
         super(Process, self).start()
-        if self.process and self.process.status in [-1,# not started
-                                                    9 #recently killed
-                                                    ]:
+        restart_statuses [ -1, # not started
+                            9, # recently killed
+                          ]
+        if getattr(self.process, 'status') in restart_statuses:
+            # can't do this without a check because play() uses iterate upstream
             self.iterate()
