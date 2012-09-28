6f8d8f7a64cc9f0242ee4fdcc98c4c5773e5b0aa
mvr <mvr>
2012-09-27 22:35:41 -0400

    add simple view for the nodeconf file

--- lib/cortex/services/web/resource/__init__.py
+++ lib/cortex/services/web/resource/__init__.py
@@ -27,6 +27,18 @@ from .util import get_source, classtree, alligator2paren
 
 ATOMS = ( list, tuple,  float, int, str )
 
+class ConfResource(Resource):
+    """ dumb resource to display the configuration file """
+    def __init__(self, universe):
+        self.universe = universe
+
+    def current_contents(self):
+        return open(self.universe.nodeconf_file).read()
+
+    def render_GET(self, request):
+        ctx = dict(filename=self.universe.nodeconf_file,
+                   text=self.current_contents())
+        return str(template('nodeconf').render(**ctx))
 
 class EFrame(Resource):
     def render_GET(self, request):
