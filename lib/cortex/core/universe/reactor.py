96bca6c263c487ffaf5940ea2924584989a1fbe1
mvr <mvr>
2012-11-02 12:30:48 -0400

    primitive tracking for which agents want to open which ports

--- lib/cortex/core/universe/reactor.py
+++ lib/cortex/core/universe/reactor.py
@@ -1,16 +1,32 @@
 """ cortex.core.universe.reactor
 """
 
+from multiprocessing import Process
+
 from twisted.internet import reactor
 
+from cortex.core.util import getcaller
 from cortex.core.util import report
-from multiprocessing import Process
+
 
 class ReactorAspect(object):
     reactor       = reactor
     callLater     = reactor.callLater
     listenTCP     = reactor.listenTCP
     getThreadPool = reactor.getThreadPool
+
+    def listenTCP(self, port, factory, backlog=50, interface=''):
+        """ 1) find out which agent wants to use this port,
+            2) log it in self.ports
+            3) chain to the real listenTCP
+        """
+        caller = getcaller()
+        # try getting the instance,
+        # failing that use the class
+        owner = caller.get('self', None)
+        owner = owner or caller.get('kls', 'unknown')
+        self.ports[port] += [owner]
+        return reactor.listenTCP(port, factory, backlog=backlog, interface=interface)
 
     def callInProcess(self, target, args=tuple(),
                       name='DefaultProcessName', delay=1, **kargs):
@@ -24,6 +40,7 @@ class ReactorAspect(object):
             p.join()
             self.procs.remove(p)
             report('finished with process "{0}" ({1} left)'.format(p.name,len(self.procs)))
+
         def go():
             start()
             finish()
