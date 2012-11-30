ec39d63a3776201e73a3d7d6152e095e96ce7e2f
mvr <matthewvonrocketstein@gmail-dot-com>
2012-11-30 05:06:23 -0500

    cleaner with psutil

--- lib/cortex/bin/panic.py
+++ lib/cortex/bin/panic.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
 import os
+import psutil
 
 def entry():
     """ think about it.. this is not cortex --panic for a good reason."""
     print '-'*80
-    _procs = "ps aux|grep cortex|grep \"sh -c\""
-    _procs = _procs.format(user=os.environ['USER'])
-    procs = os.popen(_procs).read()
+    procs = [x for x in psutil.process_iter() if 'cortex' in x.name]
     print procs
-    print '-'*80
-    pids =  os.popen(_procs+"|awk '{print $2}'").read().split()
-    print pids
-    _pids = ' '.join(pids)
-    print os.popen('kill -KILL ' + _pids).read()
+    pids = [x.pid for x in procs]
+    [ os.system('kill -KILL {0}'.format(pid)) for pid in pids ]
+    return
