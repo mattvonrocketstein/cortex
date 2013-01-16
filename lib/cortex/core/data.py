f97beeb0979f2614d04a0138b02fb3fbbb7efcbf
matt <mvr@laptop>
2013-01-16 16:58:51 -0500

    refactor

--- lib/cortex/core/data.py
+++ lib/cortex/core/data.py
@@ -66,8 +66,14 @@ class _session(object):
             except ValueError:
                 pass
 
+# Typical instructions for loading services into the universe
+LOAD_LINDA        = [ "load_service", ("_linda",),     {} ]
+LOAD_POST_OFFICE  = [ "load_service", ("postoffice",), {} ]
+LOAD_TERMINAL     = [ "load_service", ("terminal",),   {} ]
+
+
 class _standardsession(_session):
     def __init__(self):
         super(_standardsession,self).__init__()
-        self.add_instruction([ "load_service", ("_linda",), {} ])
-        self.add_instruction([ "load_service", ("postoffice",), {} ])
+        self.add_instruction(LOAD_LINDA)
+        self.add_instruction(LOAD_POST_OFFICE)
