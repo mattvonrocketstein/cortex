43a09a352300009162616339365051f97fb3280b
mvr <matthewvonrocketstein@gmail-dot-com>
2013-01-26 09:21:50 -0500

    cleaner,simpler

--- examples/copter.py
+++ examples/copter.py
@@ -12,52 +12,62 @@ graphing options:
 
 """
 import random
+import webbrowser
 from optparse import OptionParser
+from urllib import urlencode as urlenc
 
 import cortex
-from cortex.core.universe import Universe
-from cortex.core.agent import Agent
 from cortex.core.util import report
+from cortex.core.agent import Agent
+from cortex.core.universe import Universe
 from cortex.mixins.flavors import ReactorRecursion
-from cortex.mixins import Mixin
-from channel.exceptions import ChannelExists
 
 universe = Universe
 cortex.VERBOSE = True
-
-COLLECTOR = 'collector'
-
-@ Agent.from_function #
+import urllib
+@ Agent.from_function
 def SigGen(self):
     self.value = random.random()
 
 @ Agent.from_function
 def OnReady(universe):
+    """
+        install a redirect:
+          "/demo" -> <multiplotterUrl>
+        install data-streams:
+          from each agent to /x and /y
+        compute the absolute url and open it in a webbrowser
+  """
     web = (universe|'web')
-    web.make_redirect('a',
-                      ('multiplot?endpoint=/x&'
-                       'title=xxx&endpoint=/y&'
-                       'title=yyy'))
-    x_axis = universe.agents['Axis1'].obj
-    y_axis = universe.agents['Axis2'].obj
-    web.make_data_stream('x', lambda: x_axis.value)
-    web.make_data_stream('y', lambda: y_axis.value)
+    root = web.children()[0]
+    x_axis_args = urlenc(dict(endpoint='/x', title='agent_1'))
+    y_axis_args = urlenc(dict(endpoint='/y', title='agent2'))
+    full_url = 'multiplot?' + x_axis_args + '&' + y_axis_args
+    demo_url = 'demo'
+    web.make_redirect(demo_url, full_url)
+    get_x = lambda: (universe**'Axis1').value
+    get_y = lambda: (universe**'Axis2').value
+    web.make_data_stream('x', get_x)
+    web.make_data_stream('y', get_y)
+    demo_url = 'http://{0}:{1}/{2}'.\
+               format(universe.host,
+                      universe.port_for(root),
+                      demo_url)
+    webbrowser.open_new_tab(demo_url)
 
 AXIS_1 = Agent.using(template=SigGen, flavor=ReactorRecursion)
 AXIS_2 = Agent.using(template=SigGen, flavor=ReactorRecursion)
-
 AXIS_1.period = 2
-#AXIS_2.period = .5
 
 # Order matters here
-universe.agents.manage('Axis1', kls=AXIS_1, kls_kargs={})
-universe.agents.manage('Axis2', kls=AXIS_2, kls_kargs={})
+universe.agents.manage('Axis1',   kls=AXIS_1,  kls_kargs={})
+universe.agents.manage('Axis2',   kls=AXIS_2,  kls_kargs={})
 universe.agents.manage('OnReady', kls=OnReady, kls_kargs={})
 
-universe.set_nodes([["load_service", "postoffice"],
-                    ["load_service", "network_mapper"],
-                    ["load_service", "web"],
-                    ["load_service", "api"],
-                    ["load_service", "terminal"], ])
+default_nodes = [ ["load_service", "web"],
+                  ["load_service", "api"],
+                  ["load_service", "terminal"],
+                  ["load_service", "postoffice"],
+                  ["load_service", "network_mapper"], ]
+universe.set_nodes(default_nodes)
 universe.play()
-#
