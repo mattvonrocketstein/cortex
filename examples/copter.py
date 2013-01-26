a98edfdb161ac127fa64e438afead3cf5a960303
mvr <matthewvonrocketstein@gmail-dot-com>
2013-01-25 20:21:04 -0500

    simpler

--- examples/copter.py
+++ examples/copter.py
@@ -21,28 +21,23 @@ from cortex.core.util import report
 from cortex.mixins.flavors import ReactorRecursion
 from cortex.mixins import Mixin
 from channel.exceptions import ChannelExists
+
 universe = Universe
-cortex.VERBOSE = True
+#cortex.VERBOSE = True
+
 COLLECTOR = 'collector'
 
 class SigGen(Mixin):
-    """ generate a stream of random numbers and shout them out """
-
     period = 1
-
-    def get_value(self):
-        return self.value
-
-    def iterate(self):
-        self.value = random.random()
+    def iterate(self):   self.value = random.random()
 
 @ Agent.from_function
 def OnReady(universe):
     web = (universe|'web')
     x = universe.agents['Axis1'].obj
     y = universe.agents['Axis2'].obj
-    web.make_data_stream('x', x.get_value)
-    web.make_data_stream('y', y.get_value)
+    web.make_data_stream('x', lambda: x.value)
+    web.make_data_stream('y', lambda: y.value)
 
 AXIS_1 = Agent.using(template=SigGen, flavor=ReactorRecursion)
 AXIS_2 = Agent.using(template=SigGen, flavor=ReactorRecursion)
