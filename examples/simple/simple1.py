feece15b514b039c7384286d6020123bd8eeb7ff
mvr <matthewvonrocketstein@gmail-dot-com>
2013-01-27 10:40:06 -0500

    simple demos

---
+++ examples/simple/simple1.py
@@ -0,0 +1,95 @@
+""" this is the Simple Demo, version 1. run this file with python directly.
+
+    all versions of the Simple Demo are equivalent,
+    but show various ways of architecting the solution.
+"""
+import random
+import webbrowser
+
+import cortex
+from cortex.core.agent import Agent
+from cortex.core.universe import Universe
+from cortex.mixins.flavors import ReactorRecursion
+
+cortex.VERBOSE = True
+
+# set up enough services to support this demo and allow user to inspect it
+Universe.set_nodes([ ["load_service", "web"],
+                     ["load_service", "terminal"],
+                     ["load_service", "postoffice"], ])
+
+# create a simple Agent template with no concurrency.  agent's of this
+# type will generate a random signal of values in [0,1].  we will defer
+# the decision of how to actually run this agent, and just say that here
+# is something that happens repeatedly when it runs.
+@ Agent.from_function
+def SigGen(self):
+    self.value = random.random()
+
+# create a different kind of agent template.  this agent will run be
+# registered last so it will bootstrap last, and here we'll only need
+# one instance of the final product.
+@ Agent.from_function
+def OnReady(universe):
+    # ask the universe to give us a handle for the web service
+    web = (universe|'web')
+
+    # create a multiplot instance
+    multiplot = web.new_multiplot()
+
+    # this factory takes in an agent-name and return a function
+    # which can be polled to return the current signal-value
+    datagen_factory = lambda name: lambda: (universe**name).value
+
+    # isolate only the signal-generator agents and iterate over them.
+    # for each one we make the multiplot aware of names (which will be
+    # translated to url-endpoints) and a method for converting that name
+    # to a function which can return measurements on the corresponding
+    # signal.
+    signal_generators = universe.agents.filter_by_type(SigGen)
+    for signal_generating_agent in signal_generators:
+        name = signal_generating_agent.name
+        multiplot.install_subplot(name, datagen_factory(name))
+
+    # everything has been registered with the multiplot, so now connect
+    # the endpoints.  all url endpoints will be connected to the generators
+    multiplot.install_streams()
+
+    # the multiplot url is safe to access now that the instance is
+    # totally configured, but that url is complex with a disgusting
+    # large querystring.  for convenience, the next line makes a redirect
+    # from '/demo', and opens a webbrowser there.
+    _, short_url = web.make_redirect('demo', multiplot.url)
+    webbrowser.open_new_tab(short_url)
+
+# a list of unique agent-iteration intervals.
+# we'll end up creating one agent for each speed.
+AGENT_ITERATION_SPEEDS = [.1, 1, 3]
+
+for speed in AGENT_ITERATION_SPEEDS:
+    # create a unique name for each unique speed
+    agent_name = 'Agent' + str(speed)
+    # create a new type of Agent, binding both
+    # the template and a specific flavor of autonomy.
+    # the iteration speed will affect the specifics
+    # of the autonomy.
+    AgentKlass = Agent.using(template=SigGen,
+                             flavor=ReactorRecursion)
+
+    AgentKlass.period = speed
+
+    # this is essentially a deferred instantiation of the
+    # agent.  (we can't instantiate it because that's the
+    # job of the universe once it's bootstrapped).  we
+    # register the agent type with a universe, and give the
+    # name the instance will use.
+    Universe.agents.manage(agent_name, kls=AgentKlass)
+
+# another request for registration/delayed instantiation.
+# as mentioned above it's definiton, the OnReady agent will
+# bootstrap our setup after the universe itself is finished
+# boostrapping.
+Universe.agents.manage('OnReady', kls=OnReady)
+
+# everything is ready.  hit the button and start the universe
+Universe.play()
--- examples/simple/simple2.py
+++ examples/simple/simple2.py
@@ -1,4 +1,4 @@
-""" this is the Simple Demo, version 1. run this file with python directly.
+""" this is the Simple Demo, version 2. run this file with python directly.
 
     all versions of the Simple Demo are equivalent,
     but show various ways of architecting the solution.
@@ -11,11 +11,10 @@ from cortex.core.agent import Agent
 from cortex.core.universe import Universe
 from cortex.mixins.flavors import ReactorRecursion
 
-universe = Universe
 cortex.VERBOSE = True
 
 # set up enough services to support this demo and allow user to inspect it
-universe.set_nodes([ ["load_service", "web"],
+Universe.set_nodes([ ["load_service", "web"],
                      ["load_service", "terminal"],
                      ["load_service", "postoffice"], ])
 
@@ -23,45 +22,46 @@ universe.set_nodes([ ["load_service", "web"],
 # type will generate a random signal of values in [0,1].  we will defer
 # the decision of how to actually run this agent, and just say that here
 # is something that happens repeatedly when it runs.
-@ Agent.from_function
-def SigGen(self):
-    self.value = random.random()
+class SigGen(Agent):
+    def iterate(self):
+        self.value = random.random()
 
 # create a different kind of agent template.  this agent will run be
 # registered last so it will bootstrap last, and here we'll only need
 # one instance of the final product.
-@ Agent.from_function
-def OnReady(universe):
-    # ask the universe to give us a handle for the web service
-    web = (universe|'web')
+class OnReady(Agent):
+    def iterate(self):
+        universe = self.universe
+        # ask the universe to give us a handle for the web service
+        web = (universe|'web')
 
-    # create a multiplot instance
-    multiplot = web.new_multiplot()
+        # create a multiplot instance
+        multiplot = web.new_multiplot()
 
-    # this factory takes in an agent-name and return a function
-    # which can be polled to return the current signal-value
-    datagen_factory = lambda name: lambda: (universe**name).value
+        # this factory takes in an agent-name and return a function
+        # which can be polled to return the current signal-value
+        datagen_factory = lambda name: lambda: (universe**name).value
 
-    # isolate only the signal-generator agents and iterate over them.
-    # for each one we make the multiplot aware of names (which will be
-    # translated to url-endpoints) and a method for converting that name
-    # to a function which can return measurements on the corresponding
-    # signal.
-    signal_generators = universe.agents.filter_by_type(SigGen)
-    for signal_generating_agent in signal_generators:
-        name = signal_generating_agent.name
-        multiplot.install_subplot(name, datagen_factory(name))
+        # isolate only the signal-generator agents and iterate over them.
+        # for each one we make the multiplot aware of names (which will be
+        # translated to url-endpoints) and a method for converting that name
+        # to a function which can return measurements on the corresponding
+        # signal.
+        signal_generators = universe.agents.filter_by_type(SigGen)
+        for signal_generating_agent in signal_generators:
+            name = signal_generating_agent.name
+            multiplot.install_subplot(name, datagen_factory(name))
 
-    # everything has been registered with the multiplot, so now connect
-    # the endpoints.  all url endpoints will be connected to the generators
-    multiplot.install_streams()
+        # everything has been registered with the multiplot, so now connect
+        # the endpoints.  all url endpoints will be connected to the generators
+        multiplot.install_streams()
 
-    # the multiplot url is safe to access now that the instance is
-    # totally configured, but that url is complex with a disgusting
-    # large querystring.  for convenience, the next line makes a redirect
-    # from '/demo', and opens a webbrowser there.
-    _, short_url = web.make_redirect('demo', multiplot.url)
-    webbrowser.open_new_tab(short_url)
+        # the multiplot url is safe to access now that the instance is
+        # totally configured, but that url is complex with a disgusting
+        # large querystring.  for convenience, the next line makes a redirect
+        # from '/demo', and opens a webbrowser there.
+        _, short_url = web.make_redirect('demo', multiplot.url)
+        webbrowser.open_new_tab(short_url)
 
 # a list of unique agent-iteration intervals.
 # we'll end up creating one agent for each speed.
@@ -84,13 +84,13 @@ for speed in AGENT_ITERATION_SPEEDS:
     # job of the universe once it's bootstrapped).  we
     # register the agent type with a universe, and give the
     # name the instance will use.
-    universe.agents.manage(agent_name, kls=AgentKlass)
+    Universe.agents.manage(agent_name, kls=AgentKlass)
 
 # another request for registration/delayed instantiation.
 # as mentioned above it's definiton, the OnReady agent will
 # bootstrap our setup after the universe itself is finished
 # boostrapping.
-universe.agents.manage('OnReady', kls=OnReady)
+Universe.agents.manage('OnReady', kls=OnReady)
 
 # everything is ready.  hit the button and start the universe
-universe.play()
+Universe.play()
